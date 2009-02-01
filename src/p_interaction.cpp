c11f1a7fdc3233ba6697ed5b6744c1dfa1b9f82a
Christoph Oelckers <coelckers@zdoom.fake>
2009-02-01 09:11:05 +0000

    - Fixed: The damage factor for 'normal' damage is supposed to be applied to   all damage types that don't have a specific damage factor.
    SVN r1388 (trunk)

--- src/p_interaction.cpp
+++ src/p_interaction.cpp
@@ -957,6 +957,7 @@ void P_DamageMobj (AActor *target, AActor *inflictor, AActor *source, int damage
 	if (df != NULL)
 	{
 		fixed_t * pdf = df->CheckKey(mod);
+		if (pdf== NULL && mod != NAME_None) pdf = df->CheckKey(NAME_None);
 		if (pdf != NULL)
 		{
 			damage = FixedMul(damage, *pdf);

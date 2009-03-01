74642ca6263dcd6b80052a0cfe3dc89e964f5ab0
Christoph Oelckers <coelckers@zdoom.fake>
2009-03-01 10:12:12 +0000

    - Fixed: AActor::SetOrigin must call P_FindFloorCeiling to get the true   floor and ceiling heights. Otherwise the actor will fall right through   3DMidtex textures (and 3D-floors.)
    SVN r1456 (trunk)

--- src/p_maputl.cpp
+++ src/p_maputl.cpp
@@ -564,6 +564,7 @@ void AActor::SetOrigin (fixed_t ix, fixed_t iy, fixed_t iz)
 	LinkToWorld ();
 	floorz = Sector->floorplane.ZatPoint (ix, iy);
 	ceilingz = Sector->ceilingplane.ZatPoint (ix, iy);
+	P_FindFloorCeiling(this, true);
 }
 
 FBlockNode *FBlockNode::FreeBlocks = NULL;

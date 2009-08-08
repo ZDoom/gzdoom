ae21fa45620d4f8e7208728f06659b0c8eba6b92
Randy Heit <rheit@zdoom.fake>
2009-08-08 22:52:58 +0000

    - Changed the return value of SetActivatorToTarget to match the description   in the wiki.
    SVN r1764 (trunk)

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -2951,8 +2951,9 @@ int DLevelScript::CallFunction(int argCount, int funcIndex, SDWORD *args)
 			if (actor != NULL)
 			{
 				activator = actor;
+				return 1;
 			}
-			return activator != NULL;
+			return 0;
 
 		case ACSF_GetActorViewHeight:
 			actor = SingleActorFromTID(args[0], NULL);

4edd51eb098f11d244500d4b95ac4f73657c518a
Randy Heit <rheit@zdoom.fake>
2009-11-29 01:56:22 +0000

    - Use the spawned class's scale as default for ScriptedMarine instead of   DoomPlayer.
    SVN r2006 (trunk)

--- src/g_doom/a_scriptedmarine.cpp
+++ src/g_doom/a_scriptedmarine.cpp
@@ -97,14 +97,6 @@ void AScriptedMarine::BeginPlay ()
 			}
 		}
 	}
-
-	// Copy the standard player's scaling
-	AActor * playerdef = GetDefaultByName("DoomPlayer");
-	if (playerdef != NULL)
-	{
-		scaleX = playerdef->scaleX;
-		scaleY = playerdef->scaleY;
-	}
 }
 
 void AScriptedMarine::Tick ()
@@ -607,14 +599,12 @@ void AScriptedMarine::SetSprite (const PClass *source)
 	if (source == NULL || source->ActorInfo == NULL)
 	{ // A valid actor class wasn't passed, so use the standard sprite
 		SpriteOverride = sprite = GetClass()->ActorInfo->OwnedStates[0].sprite;
-		// Copy the standard player's scaling
-		AActor * playerdef = GetDefaultByName("DoomPlayer");
-		if (playerdef == NULL) playerdef = GetDefaultByType(RUNTIME_CLASS(AScriptedMarine));
-		scaleX = playerdef->scaleX;
-		scaleY = playerdef->scaleY;
+		// Copy the standard scaling
+		scaleX = GetDefault()->scaleX;
+		scaleY = GetDefault()->scaleY;
 	}
 	else
-	{ // Use the same sprite the passed class spawns with
+	{ // Use the same sprite and scaling the passed class spawns with
 		SpriteOverride = sprite = GetDefaultByType (source)->SpawnState->sprite;
 		scaleX = GetDefaultByType(source)->scaleX;
 		scaleY = GetDefaultByType(source)->scaleY;

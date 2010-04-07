d6d2ce6b2400acb77f931e68dfb6511704e75d91
Randy Heit <rheit@zdoom.fake>
2010-04-07 02:16:51 +0000

    - Fixed: maxitems calculation for MKEY_Up in M_OptButtonHandler() used the unscaled text height   instead of the scaled text height to calculate the bottom-most usable row.
    SVN r2275 (trunk)

--- src/m_options.cpp
+++ src/m_options.cpp
@@ -2319,7 +2319,7 @@ void M_OptButtonHandler(EMenuKey key, bool repeat)
 					}
 					ytop *= CleanYfac_1;
 					rowheight *= CleanYfac_1;
-					maxitems = (screen->GetHeight() - SmallFont->GetHeight() - ytop) / rowheight + 1;
+					maxitems = (screen->GetHeight() - rowheight - ytop) / rowheight + 1;
 
 					CurrentMenu->scrollpos = MAX (0,CurrentMenu->numitems - maxitems + CurrentMenu->scrolltop);
 					CurrentItem = CurrentMenu->numitems - 1;

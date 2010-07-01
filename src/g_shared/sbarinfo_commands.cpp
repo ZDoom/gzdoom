e1e7cebd01bbec0e505e2f24b3bd80f15bf52f0c
Christoph Oelckers <coelckers@zdoom.fake>
2010-07-01 20:57:11 +0000

    - fixed: The SBARINFO parser insisted that all player classes being specified have to exist. This, however, made it impossible to redefine the player classes in Hexen.
    SVN r2401 (trunk)

--- src/g_shared/sbarinfo_commands.cpp
+++ src/g_shared/sbarinfo_commands.cpp
@@ -2398,8 +2398,10 @@ class CommandPlayerClass : public SBarInfoCommandFlowControl
 						break;
 					}
 				}
+				/*
 				if(!foundClass)
 					sc.ScriptError("Unkown PlayerClass '%s'.", sc.String);
+				*/
 				if(!sc.CheckToken(','))
 					break;
 			}

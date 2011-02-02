bb67f6d0a2dfcb4b4995fc9c373a6c879b19ee95
Randy Heit <rheit@zdoom.fake>
2011-02-02 02:11:15 +0000

    - Add ~/.zdoom as a default path in [FileSearch.Directories] on Unix systems.
    SVN r3131 (trunk)

--- src/gameconfigfile.cpp
+++ src/gameconfigfile.cpp
@@ -154,6 +154,7 @@ FGameConfigFile::FGameConfigFile ()
 #elif !defined(unix)
 		SetValueForKey ("Path", "$PROGDIR", true);
 #else
+		SetValueForKey ("Path", "~/" GAME_DIR, true);
 		SetValueForKey ("Path", SHARE_DIR, true);
 #endif
 		SetValueForKey ("Path", "$DOOMWADDIR", true);

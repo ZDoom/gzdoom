307050d38dada3e3a4da9b96672d5be695602e06
Randy Heit <rheit@zdoom.fake>
2011-10-03 00:56:01 +0000

    - Remove the chasecam restriction that the player sprite must be visible. (When   crushed, the real player is turned invisible, defeating the death/chasecam.)
    SVN r3300 (trunk)

--- src/r_utility.cpp
+++ src/r_utility.cpp
@@ -773,10 +773,7 @@ void R_SetupFrame (AActor *actor)
 	}
 
 	if (player != NULL && gamestate != GS_TITLELEVEL &&
-		((player->cheats & CF_CHASECAM) || (r_deathcamera && camera->health <= 0)) &&
-		(camera->RenderStyle.BlendOp != STYLEOP_None) &&
-		!(camera->renderflags & RF_INVISIBLE) &&
-		camera->sprite != SPR_TNT1)
+		((player->cheats & CF_CHASECAM) || (r_deathcamera && camera->health <= 0)))
 	{
 		// [RH] Use chasecam view
 		P_AimCamera (camera, iview->nviewx, iview->nviewy, iview->nviewz, viewsector);

111b5c60c173704683db01dfd14771f2e74dbe25
Randy Heit <rheit@zdoom.fake>
2012-09-01 22:34:09 +0000

    - Fixed: The check in P_SpawnBlood() to avoid advancing the state to something not owned by   the spawned blood actor was backwards and would only advance to state's NOT owned by it.
    SVN r3861 (trunk)

--- src/p_mobj.cpp
+++ src/p_mobj.cpp
@@ -4864,14 +4864,15 @@ void P_SpawnBlood (fixed_t x, fixed_t y, fixed_t z, angle_t dir, int damage, AAc
 			while (cls != RUNTIME_CLASS(AActor))
 			{
 				FActorInfo *ai = cls->ActorInfo;
+				int checked_advance = advance;
 				if (ai->OwnsState(th->SpawnState))
 				{
-					for (; advance > 0; --advance)
+					for (; checked_advance > 0; --checked_advance)
 					{
 						// [RH] Do not set to a state we do not own.
-						if (!ai->OwnsState(th->SpawnState + advance))
+						if (ai->OwnsState(th->SpawnState + checked_advance))
 						{
-							th->SetState(th->SpawnState + advance);
+							th->SetState(th->SpawnState + checked_advance);
 							goto statedone;
 						}
 					}

11862ac06ba673e52f8306596fd661042355d5c9
Randy Heit <rheit@zdoom.fake>
2013-01-23 04:04:21 +0000

    - Sync A_LookEx with A_Look, with regards to COMPATF_SOUNDTARGET.
    SVN r4036 (trunk)

--- src/p_enemy.cpp
+++ src/p_enemy.cpp
@@ -1868,7 +1868,8 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_LookEx)
 	{
 		if (!(flags & LOF_NOSOUNDCHECK))
 		{
-			targ = (self->flags & MF_NOSECTOR)? self->Sector->SoundTarget : self->LastHeard;
+			targ = (i_compatflags & COMPATF_SOUNDTARGET || self->flags & MF_NOSECTOR)?
+				self->Sector->SoundTarget : self->LastHeard;
 			if (targ != NULL)
 			{
 				// [RH] If the soundtarget is dead, don't chase it

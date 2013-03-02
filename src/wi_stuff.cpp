8f7cd73a41ae5d43dbb9e15a89809a03ca4a204e
Randy Heit <rheit@zdoom.fake>
2013-03-02 01:27:36 +0000

    - Comment out star and bstar stuff in wi_stuff.cpp.
    SVN r4171 (trunk)

--- src/wi_stuff.cpp
+++ src/wi_stuff.cpp
@@ -253,8 +253,8 @@ static FTexture* 		sucks;
 static FTexture* 		killers;	// "killers", "victims"
 static FTexture* 		victims;
 static FTexture* 		total;		// "Total", your face, your dead face
-static FTexture* 		star;
-static FTexture* 		bstar;
+//static FTexture* 		star;
+//static FTexture* 		bstar;
 static FTexture* 		p;			// Player graphic
 static FTexture*		lnames[2];	// Name graphics of each level (centered)
 
@@ -1995,10 +1995,11 @@ void WI_loadData(void)
 		killers = TexMan["WIKILRS"];	// "killers" (vertical]
 		victims = TexMan["WIVCTMS"];	// "victims" (horiz]
 		total = TexMan["WIMSTT"];		// "total"
-		star = TexMan["STFST01"];		// your face
-		bstar = TexMan["STFDEAD0"];		// dead face
+//		star = TexMan["STFST01"];		// your face
+//		bstar = TexMan["STFDEAD0"];		// dead face
  		p = TexMan["STPBANY"];
 	}
+#if 0
 	else if (gameinfo.gametype & GAME_Raven)
 	{
 		if (gameinfo.gametype == GAME_Heretic)
@@ -2017,6 +2018,7 @@ void WI_loadData(void)
 		star = BigFont->GetChar('*', NULL);
 		bstar = star;
 	}
+#endif
 
 	// Use the local level structure which can be overridden by hubs
 	lnametexts[0] = level.LevelName;		

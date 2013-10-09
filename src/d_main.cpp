3c376aa342994971e0e3f578476e8085d346cdf1
Randy Heit <rheit@zdoom.fake>
2013-10-08 20:18:35 -0500

    Move C_ExecCmdLineParams() call slightly later in the startup process.
    - Fixed: You could not set any CVARINFO-defined cvars from the command line
      because command line console commands were executed before wads were
      even loaded. Off the top of my head, I can't think of anything that would\
      break by having them get executed after wads are loaded.

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -2281,8 +2281,6 @@ void D_DoomMain (void)
 		execFiles = Args->GatherFiles ("-exec");
 		D_MultiExec (execFiles, true);
 
-		C_ExecCmdLineParams ();		// [RH] do all +set commands on the command line
-
 		CopyFiles(allwads, pwads);
 
 		// Since this function will never leave we must delete this array here manually.
@@ -2297,6 +2295,8 @@ void D_DoomMain (void)
 
 		// Now that wads are loaded, define mod-specific cvars.
 		ParseCVarInfo();
+
+		C_ExecCmdLineParams ();		// [RH] do all +set commands on the command line
 
 		// [RH] Initialize localizable strings.
 		GStrings.LoadStrings (false);

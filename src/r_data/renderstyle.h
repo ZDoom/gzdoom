c959b3f250386a239521e63f74b1c4e897fbfa36
Christoph Oelckers <coelckers@zdoom.fake>
2013-12-05 15:28:09 +0100

    - added 'subtract' render style to DECORATE parser. This is actually 'reverse subtract'

--- src/r_data/renderstyle.h
+++ src/r_data/renderstyle.h
@@ -49,6 +49,7 @@ enum ERenderStyle
 	STYLE_Shaded,			// Treat patch data as alpha values for alphacolor
 	STYLE_TranslucentStencil,
 	STYLE_Shadow,
+	STYLE_Subtract,			// Actually this is 'reverse subtract' but this is what normal people would expect by 'subtract'.
 
 	STYLE_Count
 };

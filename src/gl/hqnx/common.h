0aba6e09f3c53b012e0da38552e42253935aed52
Gaerzi <gaerzi@gmail.com>
2014-04-03 20:14:38 +0200

    Disambiguate which abs type we want.

--- src/gl/hqnx/common.h
+++ src/gl/hqnx/common.h
@@ -48,9 +48,9 @@ static inline uint32_t rgb_to_yuv(uint32_t c)
 
 /* Test if there is difference in color */
 static inline int yuv_diff(uint32_t yuv1, uint32_t yuv2) {
-    return (( abs((yuv1 & Ymask) - (yuv2 & Ymask)) > trY ) ||
-            ( abs((yuv1 & Umask) - (yuv2 & Umask)) > trU ) ||
-            ( abs((yuv1 & Vmask) - (yuv2 & Vmask)) > trV ) );
+    return (( abs((int64_t)(yuv1 & Ymask) - (int64_t)(yuv2 & Ymask)) > trY ) ||
+            ( abs((int64_t)(yuv1 & Umask) - (int64_t)(yuv2 & Umask)) > trU ) ||
+            ( abs((int64_t)(yuv1 & Vmask) - (int64_t)(yuv2 & Vmask)) > trV ) );
 }
 
 static inline int Diff(uint32_t c1, uint32_t c2)

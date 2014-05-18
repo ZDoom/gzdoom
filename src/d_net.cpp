26b1abe3dab611001b49ce717853b0accd8c5516
Edward Richardson <Edward850@crantime.org>
2014-05-18 18:37:34 +1200

    Fix netgame arbitration with long map lump names

--- src/d_net.cpp
+++ src/d_net.cpp
@@ -1385,7 +1385,7 @@ bool DoArbitrate (void *userdata)
 
 			stream = &netbuffer[4];
 			s = ReadString (&stream);
-			startmap = FString(s, 8);
+			startmap = FString(s, strlen(s));
 			delete[] s;
 			rngseed = ReadLong (&stream);
 			C_ReadCVars (&stream);

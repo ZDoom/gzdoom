77d9d9b2a588c5902dfb26ebb8affe3fdab2bdd7
Christoph Oelckers <coelckers@zdoom.fake>
2014-07-27 11:53:18 +0200

    - fixed: For updating the model VAO's attribute pointers it is necessary to first bind the vertex buffer we need to refer to, because this is not part of the VAO's state.

--- src/gl/models/gl_models.cpp
+++ src/gl/models/gl_models.cpp
@@ -140,6 +140,7 @@ FModelVertexBuffer::~FModelVertexBuffer()
 
 unsigned int FModelVertexBuffer::SetupFrame(unsigned int frame1, unsigned int frame2, float factor)
 {
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
 	glVertexAttribPointer(VATTR_VERTEX, 3, GL_FLOAT, false, sizeof(FModelVertex), &VMO[frame1].x);
 	glVertexAttribPointer(VATTR_TEXCOORD, 2, GL_FLOAT, false, sizeof(FModelVertex), &VMO[frame1].u);
 	glVertexAttribPointer(VATTR_VERTEX2, 3, GL_FLOAT, false, sizeof(FModelVertex), &VMO[frame2].x);

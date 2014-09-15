8f924bba0c8983a375741ad6a50df2171d196828
Christoph Oelckers <coelckers@zdoom.fake>
2014-09-15 11:54:16 +0200

    - version 2.0.03.

--- src/version.h
+++ src/version.h
@@ -41,12 +41,12 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "2.1pre"
+#define VERSIONSTR "2.0.03"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 2,0,9999,0
-#define RC_PRODUCTVERSION 2,0,9999,0
-#define RC_PRODUCTVERSION2 "2.1pre"
+#define RC_FILEVERSION 2,0,3,0
+#define RC_PRODUCTVERSION 2,0,3,0
+#define RC_PRODUCTVERSION2 "2.0.03"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you

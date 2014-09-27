e025f4090221746fc35db4065a2d1455117d737b
Christoph Oelckers <coelckers@zdoom.fake>
2014-09-27 08:54:18 +0200

    - more redundancy removal: Consolidated the common part of the A_Kill* functions into a subfunction.

--- src/thingdef/thingdef_codeptr.cpp
+++ src/thingdef/thingdef_codeptr.cpp
@@ -2675,133 +2675,6 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_JumpIf)
 
 }
 
-enum KILS
-{
-	KILS_FOILINVUL =	1 << 0,
-	KILS_KILLMISSILES = 1 << 1,
-	KILS_NOMONSTERS =	1 << 2,
-};
-
-//===========================================================================
-//
-// A_KillMaster(damagetype, int flags)
-//
-//===========================================================================
-DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_KillMaster)
-{
-	ACTION_PARAM_START(2);
-	ACTION_PARAM_NAME(damagetype, 0);
-	ACTION_PARAM_INT(flags, 1);
-
-	if (self->master != NULL)
-	{
-		if ((self->master->flags & MF_MISSILE) && (flags & KILS_KILLMISSILES))
-		{
-			//[MC] Now that missiles can set masters, lets put in a check to properly destroy projectiles. BUT FIRST! New feature~!
-			//Check to see if it's invulnerable. Disregarded if foilinvul is on, but never works on a missile with NODAMAGE
-			//since that's the whole point of it.
-			if ((!(self->master->flags2 & MF2_INVULNERABLE) || (flags & KILS_FOILINVUL)) && !(self->master->flags5 & MF5_NODAMAGE))
-			{
-				P_ExplodeMissile(self->master, NULL, NULL);
-			}
-		}
-		if (!(flags & KILS_NOMONSTERS))
-		{
-			if (flags & KILS_FOILINVUL)
-			{
-				P_DamageMobj(self->master, self, self, self->master->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR | DMG_FOILINVUL);
-			}
-			else
-			{
-				P_DamageMobj(self->master, self, self, self->master->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR);
-			}
-		}		
-	}
-}
-
-//===========================================================================
-//
-// A_KillChildren(damagetype, int flags)
-//
-//===========================================================================
-DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_KillChildren)
-{
-	ACTION_PARAM_START(2);
-	ACTION_PARAM_NAME(damagetype, 0);
-	ACTION_PARAM_INT(flags, 1);
-
-	TThinkerIterator<AActor> it;
-	AActor *mo;
-
-	while ( (mo = it.Next()) )
-	{
-		if (mo->master == self)
-		{
-			if ((mo->flags & MF_MISSILE) && (flags & KILS_KILLMISSILES))
-			{
-				if ((!(mo->flags2 & MF2_INVULNERABLE) || (flags & KILS_FOILINVUL)) && !(mo->flags5 & MF5_NODAMAGE))
-				{
-					P_ExplodeMissile(mo, NULL, NULL);
-				}
-			}
-			if (!(flags & KILS_NOMONSTERS))
-			{
-				if (flags & KILS_FOILINVUL)
-				{
-					P_DamageMobj(mo, self, self, mo->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR | DMG_FOILINVUL);
-				}
-				else
-				{
-					P_DamageMobj(mo, self, self, mo->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR);
-				}
-			}
-		}
-	}
-}
-
-//===========================================================================
-//
-// A_KillSiblings(damagetype, int flags)
-//
-//===========================================================================
-DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_KillSiblings)
-{
-	ACTION_PARAM_START(2);
-	ACTION_PARAM_NAME(damagetype, 0);
-	ACTION_PARAM_INT(flags, 1);
-
-	TThinkerIterator<AActor> it;
-	AActor *mo;
-
-	if (self->master != NULL)
-	{
-		while ( (mo = it.Next()) )
-		{
-			if (mo->master == self->master && mo != self)
-			{
-				if ((mo->flags & MF_MISSILE) && (flags & KILS_KILLMISSILES))
-				{
-					if ((!(mo->flags2 & MF2_INVULNERABLE) || (flags & KILS_FOILINVUL)) && !(mo->flags5 & MF5_NODAMAGE))
-					{
-						P_ExplodeMissile(mo, NULL, NULL);
-					}
-				}
-				if (!(flags & KILS_NOMONSTERS))
-				{
-					if (flags & KILS_FOILINVUL)
-					{
-						P_DamageMobj(mo, self, self, mo->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR | DMG_FOILINVUL);
-					}
-					else
-					{
-						P_DamageMobj(mo, self, self, mo->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR);
-					}
-				}
-			}
-		}
-	}
-}
-
 //===========================================================================
 //
 // A_CountdownArg
@@ -5128,6 +5001,46 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_DamageSiblings)
 	}
 }
 
+
+//===========================================================================
+//
+// A_Kill*(damagetype, int flags)
+//
+//===========================================================================
+enum KILS
+{
+	KILS_FOILINVUL =	1 << 0,
+	KILS_KILLMISSILES = 1 << 1,
+	KILS_NOMONSTERS =	1 << 2,
+};
+
+static void DoKill(AActor *killtarget, AActor *self, FName damagetype, int flags)
+{
+	if ((killtarget->flags & MF_MISSILE) && (flags & KILS_KILLMISSILES))
+	{
+		//[MC] Now that missiles can set masters, lets put in a check to properly destroy projectiles. BUT FIRST! New feature~!
+		//Check to see if it's invulnerable. Disregarded if foilinvul is on, but never works on a missile with NODAMAGE
+		//since that's the whole point of it.
+		if ((!(killtarget->flags2 & MF2_INVULNERABLE) || (flags & KILS_FOILINVUL)) && !(killtarget->flags5 & MF5_NODAMAGE))
+		{
+			P_ExplodeMissile(self->target, NULL, NULL);
+		}
+	}
+	if (!(flags & KILS_NOMONSTERS))
+	{
+		if (flags & KILS_FOILINVUL)
+		{
+			P_DamageMobj(killtarget, self, self, killtarget->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR | DMG_FOILINVUL);
+		}
+		else
+		{
+			P_DamageMobj(killtarget, self, self, killtarget->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR);
+		}
+	}
+}
+
+
+
 //===========================================================================
 //
 // A_KillTarget(damagetype, int flags)
@@ -5139,30 +5052,7 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_KillTarget)
 	ACTION_PARAM_NAME(damagetype, 0);
 	ACTION_PARAM_INT(flags, 1);
 
-	if (self->target != NULL)
-	{
-		if ((self->target->flags & MF_MISSILE) && (flags & KILS_KILLMISSILES))
-		{
-			//[MC] Now that missiles can set masters, lets put in a check to properly destroy projectiles. BUT FIRST! New feature~!
-			//Check to see if it's invulnerable. Disregarded if foilinvul is on, but never works on a missile with NODAMAGE
-			//since that's the whole point of it.
-			if ((!(self->target->flags2 & MF2_INVULNERABLE) || (flags & KILS_FOILINVUL)) && !(self->target->flags5 & MF5_NODAMAGE))
-			{
-				P_ExplodeMissile(self->target, NULL, NULL);
-			}
-		}
-		if (!(flags & KILS_NOMONSTERS))
-		{
-			if (flags & KILS_FOILINVUL)
-			{
-				P_DamageMobj(self->target, self, self, self->target->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR | DMG_FOILINVUL);
-			}
-			else
-			{
-				P_DamageMobj(self->target, self, self, self->target->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR);
-			}
-		}
-	}
+	if (self->target != NULL) DoKill(self->target, self, damagetype, flags);
 }
 
 //===========================================================================
@@ -5176,31 +5066,66 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_KillTracer)
 	ACTION_PARAM_NAME(damagetype, 0);
 	ACTION_PARAM_INT(flags, 1);
 
-	if (self->tracer != NULL)
+	if (self->tracer != NULL) DoKill(self->tracer, self, damagetype, flags);
+}
+
+//===========================================================================
+//
+// A_KillMaster(damagetype, int flags)
+//
+//===========================================================================
+DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_KillMaster)
+{
+	ACTION_PARAM_START(2);
+	ACTION_PARAM_NAME(damagetype, 0);
+	ACTION_PARAM_INT(flags, 1);
+
+	if (self->master != NULL) DoKill(self->master, self, damagetype, flags);
+}
+
+//===========================================================================
+//
+// A_KillChildren(damagetype, int flags)
+//
+//===========================================================================
+DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_KillChildren)
+{
+	ACTION_PARAM_START(2);
+	ACTION_PARAM_NAME(damagetype, 0);
+	ACTION_PARAM_INT(flags, 1);
+
+	TThinkerIterator<AActor> it;
+	AActor *mo;
+
+	while ( (mo = it.Next()) )
 	{
-		if ((self->tracer->flags & MF_MISSILE) && (flags & KILS_KILLMISSILES))
+		if (mo->master == self) DoKill(mo, self, damagetype, flags);
+	}
+}
+
+//===========================================================================
+//
+// A_KillSiblings(damagetype, int flags)
+//
+//===========================================================================
+DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_KillSiblings)
+{
+	ACTION_PARAM_START(2);
+	ACTION_PARAM_NAME(damagetype, 0);
+	ACTION_PARAM_INT(flags, 1);
+
+	TThinkerIterator<AActor> it;
+	AActor *mo;
+
+	if (self->master != NULL)
+	{
+		while ( (mo = it.Next()) )
 		{
-			//[MC] Now that missiles can set masters, lets put in a check to properly destroy projectiles. BUT FIRST! New feature~!
-			//Check to see if it's invulnerable. Disregarded if foilinvul is on, but never works on a missile with NODAMAGE
-			//since that's the whole point of it.
-			if ((!(self->tracer->flags2 & MF2_INVULNERABLE) || (flags & KILS_FOILINVUL)) && !(self->tracer->flags5 & MF5_NODAMAGE))
-			{
-				P_ExplodeMissile(self->tracer, NULL, NULL);
-			}
-		}
-		if (!(flags & KILS_NOMONSTERS))
-		{
-			if (flags & KILS_FOILINVUL)
-			{
-				P_DamageMobj(self->tracer, self, self, self->tracer->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR | DMG_FOILINVUL);
-			}
-			else
-			{
-				P_DamageMobj(self->tracer, self, self, self->tracer->health, damagetype, DMG_NO_ARMOR | DMG_NO_FACTOR);
-			}
+			if (mo->master == self->master && mo != self) DoKill(mo, self, damagetype, flags);
 		}
 	}
 }
+
 
 //===========================================================================
 //

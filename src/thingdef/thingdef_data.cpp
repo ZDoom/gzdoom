b54b18c8c581a05a2bf60b74bafe992f6865123c
MajorCooke <paul.growney22@gmail.com>
2014-11-12 14:08:26 -0600

    -Added: CAUSEPAIN. Actors which deal damage (even if 0) while having this flag can cause other actors to trigger pain chances. -Fixed: ALLOWPAIN should not trigger pain states if the damage is 0, but still allow for infighting to occur. -Fixed: an unneeded logic call was processing damage modification when it was 0 to begin with.

--- src/thingdef/thingdef_data.cpp
+++ src/thingdef/thingdef_data.cpp
@@ -246,6 +246,7 @@ static FFlagDef ActorFlags[]=
 	DEFINE_FLAG(MF7, FOILBUDDHA, AActor, flags7),
 	DEFINE_FLAG(MF7, DONTTHRUST, AActor, flags7),
 	DEFINE_FLAG(MF7, ALLOWPAIN, AActor, flags7),
+	DEFINE_FLAG(MF7, CAUSEPAIN, AActor, flags7),
 
 	// Effect flags
 	DEFINE_FLAG(FX, VISIBILITYPULSE, AActor, effects),

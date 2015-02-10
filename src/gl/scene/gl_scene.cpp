233d3b4f89cd4145a0544853451a4d00a8f4e992
Christoph Oelckers <coelckers@zdoom.fake>
2015-02-10 20:34:10 +0100

    - added the missed parts of camera roll in gl_scene.cpp.

--- src/gl/scene/gl_scene.cpp
+++ src/gl/scene/gl_scene.cpp
@@ -885,6 +885,7 @@ sector_t * FGLRenderer::RenderViewpoint (AActor * camera, GL_IRECT * bounds, flo
 	double alen = sqrt(angx*angx + angy*angy);
 
 	mAngles.Pitch = (float)RAD2DEG(asin(angy / alen));
+	mAngles.Roll = (float)(camera->roll>>ANGLETOFINESHIFT)*360.0f/FINEANGLES; 
 
 	// Scroll the sky
 	mSky1Pos = (float)fmod(gl_frameMS * level.skyspeed1, 1024.f) * 90.f/256.f;

68fcbc0c92a5ea4c3f402a4e7f73397651f10d6e
Christoph Oelckers <c.oelckers@zdoom.fake>
2016-01-21 14:54:14 +0100

    - fixed: loading sector damage information from an old savegame could retrieve invalid data due to an uninitialized variable.

--- src/p_sectors.cpp
+++ src/p_sectors.cpp
@@ -878,6 +878,7 @@ FArchive &operator<< (FArchive &arc, secspecial_t &p)
 		int special;
 		arc << special;
 		sector_t sec;
+		memset(&sec, 0, sizeof(sec));
 		P_InitSectorSpecial(&sec, special, true);
 		sec.GetSpecial(&p);
 	}

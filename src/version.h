a7d1d72d6bc830317f7a345823ec6c1d69fad9ba
Christoph Oelckers <coelckers@zdoom.fake>
2016-02-05 12:11:02 +0100

    - increase version number.

--- src/version.h
+++ src/version.h
@@ -41,12 +41,12 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "2.1pre"
+#define VERSIONSTR "2.2pre"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 2,0,9999,0
-#define RC_PRODUCTVERSION 2,0,9999,0
-#define RC_PRODUCTVERSION2 "2.1pre"
+#define RC_FILEVERSION 2,1,9999,0
+#define RC_PRODUCTVERSION 2,1,9999,0
+#define RC_PRODUCTVERSION2 "2.2pre"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you

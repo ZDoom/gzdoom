4c2bcede6157cf30fd829d5d3a26f7cee9270c8d
Randy Heit <rheit@users.noreply.github.com>
2016-02-08 21:27:02 -0600

    Update VS2005 projects for re2c and zdoom

--- tools/re2c/src/util/c99_stdint.h
+++ tools/re2c/src/util/c99_stdint.h
@@ -1,7 +1,11 @@
 #ifndef _RE2C_UTIL_C99_STDINT_
 #define _RE2C_UTIL_C99_STDINT_
 
+#if defined(_MSC_VER) && _MSC_VER < 1500
+#include "config.msc.h"
+#else
 #include "config.h"
+#endif
 
 #if HAVE_STDINT_H
 #    include <stdint.h>

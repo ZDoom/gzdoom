a7254a4af512dd1d2ece095a9e6e40a5a821560c
Christoph Oelckers <c.oelckers@zdoom.fake>
2016-02-10 14:43:04 +0100

    - removed the conversion operators from the fixedvec structs (again)
    They must have been accidentally left in by not properly resolving a merge conflict, I guess.

--- src/actor.h
+++ src/actor.h
@@ -572,31 +572,11 @@ struct FStrifeDialogueNode;
 struct fixedvec3
 {
 	fixed_t x, y, z;
-
-	operator FVector3()
-	{
-		return FVector3(FIXED2FLOAT(x), FIXED2FLOAT(y), FIXED2FLOAT(z));
-	}
-
-	operator TVector3<double>()
-	{
-		return TVector3<double>(FIXED2DBL(x), FIXED2DBL(y), FIXED2DBL(z));
-	}
 };
 
 struct fixedvec2
 {
 	fixed_t x, y;
-
-	operator FVector2()
-	{
-		return FVector2(FIXED2FLOAT(x), FIXED2FLOAT(y));
-	}
-
-	operator TVector2<double>()
-	{
-		return TVector2<double>(FIXED2DBL(x), FIXED2DBL(y));
-	}
 };
 
 class DDropItem : public DObject

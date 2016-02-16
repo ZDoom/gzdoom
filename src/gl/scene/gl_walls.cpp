c69fd3ac80d25ba0196e1221e2a59a73131daf44
Christoph Oelckers <coelckers@zdoom.fake>
2016-02-16 10:14:37 +0100

    - removed redundant SKYBOX enum.

--- src/gl/scene/gl_walls.cpp
+++ src/gl/scene/gl_walls.cpp
@@ -176,7 +176,7 @@ void GLWall::PutPortal(int ptype)
 		if (!portal)
 		{
 			// either a regular skybox or an Eternity-style horizon
-			if (skybox->special1 != SKYBOX_MAP) portal = new GLEEHorizonPortal(skybox);
+			if (skybox->special1 != SKYBOX_SKYVIEWPOINT) portal = new GLEEHorizonPortal(skybox);
 			else portal = new GLSkyboxPortal(skybox);
 		}
 		portal->AddLine(this);

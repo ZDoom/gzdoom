bc8b48f733f4de60ed6fe01fdb4d15c51001d270
Christoph Oelckers <coelckers@zdoom.fake>
2016-02-21 01:38:41 +0100

    - fixed: The horizon portal drawer must set the camera position for the shader.
    This gets lost when several portals are drawn sequentially and this is the only portal type which needs this information but doesn't go through the normal setup.
    If this is not done, fog on the horizon plane can break.

--- src/gl/scene/gl_portal.cpp
+++ src/gl/scene/gl_portal.cpp
@@ -976,6 +976,7 @@ void GLHorizonPortal::DrawContents()
 		PortalAll.Unclock();
 		return;
 	}
+	gl_RenderState.SetCameraPos(FIXED2FLOAT(viewx), FIXED2FLOAT(viewy), FIXED2FLOAT(viewz));
 
 
 	z=FIXED2FLOAT(sp->texheight);

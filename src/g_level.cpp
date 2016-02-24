51da78ba29f693260acaee459d51224569b0ca6a
Christoph Oelckers <c.oelckers@zdoom.fake>
2016-02-24 10:35:29 +0100

    - added a compatibility option to allow multiple exits to be triggered.
    This is required by Daedalus's travel tubes which contain a faulty script with some leftover debug code.

--- src/g_level.cpp
+++ src/g_level.cpp
@@ -535,7 +535,7 @@ void G_ChangeLevel(const char *levelname, int position, int flags, int nextSkill
 		Printf (TEXTCOLOR_RED "Unloading scripts cannot exit the level again.\n");
 		return;
 	}
-	if (gameaction == ga_completed)	// do not exit multiple times.
+	if (gameaction == ga_completed && !(i_compatflags2 & COMPATF2_MULTIEXIT))	// do not exit multiple times.
 	{
 		return;
 	}

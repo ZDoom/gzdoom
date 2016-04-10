fe973ba442c8b2864d2ca2d54dd9482c47555e4e
Christoph Oelckers <c.oelckers@zdoom.fake>
2016-04-10 13:54:42 +0200

    - tried to add precompiled headers to fastmath sources as well, but VC++ doesn't seem to want to play along. So I left this stuff in but disabled the precompilation until a solution can be found.

---
+++ src/g_pch2.h
@@ -0,0 +1,22 @@
+// This is separate because the files being compiled with it use different compiler settings which may affect how the header is compiled
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <float.h>
+#include <math.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <zlib.h>
+#include <new>
+#include <algorithm>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <cassert>
+#include <direct.h>
+#include <io.h>
+#include <limits>
+#include <fcntl.h>

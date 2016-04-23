0d17459c363382d98bf2c8ab5497c0f825c66785
Christoph Oelckers <coelckers@zdoom.fake>
2016-04-23 15:47:51 +0200

    - do not disable sampler objects with -gl3.

--- src/gl/system/gl_interface.cpp
+++ src/gl/system/gl_interface.cpp
@@ -128,15 +128,15 @@ void gl_LoadExtensions()
 
 	gl.vendorstring = (char*)glGetString(GL_VENDOR);
 
+	if (gl.version >= 3.3f || CheckExtension("GL_ARB_sampler_objects"))
+	{
+		gl.flags |= RFL_SAMPLER_OBJECTS;
+	}
+
 	if (CheckExtension("GL_ARB_texture_compression")) gl.flags|=RFL_TEXTURE_COMPRESSION;
 	if (CheckExtension("GL_EXT_texture_compression_s3tc")) gl.flags|=RFL_TEXTURE_COMPRESSION_S3TC;
 	if (!Args->CheckParm("-gl3"))
 	{
-		if (gl.version >= 3.3f || CheckExtension("GL_ARB_sampler_objects"))
-		{
-			gl.flags |= RFL_SAMPLER_OBJECTS;
-		}
-
 		// don't use GL 4.x features when running in GL 3 emulation mode.
 		if (CheckExtension("GL_ARB_buffer_storage"))
 		{

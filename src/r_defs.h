2447f7bc38425e2f883cf58db825b2553ea4fde4
Christoph Oelckers <c.oelckers@zdoom.fake>
2016-04-23 13:40:02 +0200

    - renamed FTransform access functions.

--- src/r_defs.h
+++ src/r_defs.h
@@ -675,7 +675,7 @@ public:
 		planes[pos].xform.xOffs += o;
 	}
 
-	double GetXOffsetF(int pos) const
+	double GetXOffset(int pos) const
 	{
 		return planes[pos].xform.xOffs;
 	}
@@ -690,7 +690,7 @@ public:
 		planes[pos].xform.yOffs += o;
 	}
 
-	double GetYOffsetF(int pos, bool addbase = true) const
+	double GetYOffset(int pos, bool addbase = true) const
 	{
 		if (!addbase)
 		{
@@ -707,7 +707,7 @@ public:
 		planes[pos].xform.xScale = o;
 	}
 
-	double GetXScaleF(int pos) const
+	double GetXScale(int pos) const
 	{
 		return planes[pos].xform.xScale;
 	}
@@ -717,7 +717,7 @@ public:
 		planes[pos].xform.yScale = o;
 	}
 
-	double GetYScaleF(int pos) const
+	double GetYScale(int pos) const
 	{
 		return planes[pos].xform.yScale;
 	}
@@ -727,7 +727,7 @@ public:
 		planes[pos].xform.Angle = o;
 	}
 
-	DAngle GetAngleF(int pos, bool addbase = true) const
+	DAngle GetAngle(int pos, bool addbase = true) const
 	{
 		if (!addbase)
 		{

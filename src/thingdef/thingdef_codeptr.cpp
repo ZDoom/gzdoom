cc8e7f8de6a34d1ee671d2d989504df6d42ab20c
MajorCooke <paul.growney22@gmail.com>
2016-05-30 15:31:19 -0500

    Cleaning finished.

--- src/thingdef/thingdef_codeptr.cpp
+++ src/thingdef/thingdef_codeptr.cpp
@@ -1832,8 +1832,6 @@ static void FireBulletMissile(AActor *self, PClassActor *missile, AActor *puff, 
 {
 	if (self && missile && puff)
 	{
-		double x = Spawnofs_xy * angle.Cos();
-		double y = Spawnofs_xy * angle.Sin();
 		DAngle ang = self->Angles.Yaw - 90;
 		DVector2 ofs = ang.ToVector(Spawnofs_xy);
 

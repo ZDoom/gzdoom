f63635e07a428f2a198274c719a1f48821f5a7b0
Magnus Norddahl <dpjudas@users.noreply.github.com>
2016-08-19 01:11:11 +0200

    Fix RFL_INVALIDATE_BUFFER check being inverted

--- src/gl/renderer/gl_renderbuffers.cpp
+++ src/gl/renderer/gl_renderbuffers.cpp
@@ -447,7 +447,7 @@ void FGLRenderBuffers::BlitSceneToTexture()
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, mPipelineFB[mCurrentPipelineTexture]);
 	glBlitFramebuffer(0, 0, mWidth, mHeight, 0, 0, mWidth, mHeight, GL_COLOR_BUFFER_BIT, GL_NEAREST);
 
-	if ((gl.flags & RFL_INVALIDATE_BUFFER) == 0)
+	if ((gl.flags & RFL_INVALIDATE_BUFFER) != 0)
 	{
 		GLenum attachments[2] = { GL_COLOR_ATTACHMENT0, GL_DEPTH_STENCIL_ATTACHMENT };
 		glInvalidateFramebuffer(GL_READ_FRAMEBUFFER, 2, attachments);

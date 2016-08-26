465792df0a0243c8ae4cc3925c056d85db319891
Christoph Oelckers <c.oelckers@zdoom.fake>
2016-08-26 02:16:06 +0200

    - make sure that after travelling has finished, no travelling thinkers are left in the list.
    Since this list is excluded from regular thinker cleaning, anything that may survive through the end of G_FinishTravel will endlessly multiply and severely break the following savegames or just simply crash on broken pointers.

--- src/g_level.cpp
+++ src/g_level.cpp
@@ -1295,6 +1295,11 @@ void G_FinishTravel ()
 	}
 
 	bglobal.FinishTravel ();
+
+	// make sure that, after travelling has completed, no travelling thinkers are left.
+	// Since this list is excluded from regular thinker cleaning, anything that may survive through here
+	// will endlessly multiply and severely break the following savegames or just simply crash on broken pointers.
+	DThinker::DestroyThinkersInList(STAT_TRAVELLING);
 }
  
 //==========================================================================

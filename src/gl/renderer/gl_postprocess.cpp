37e3172c945298a95709e81752c783bc70b2f8c4
Magnus Norddahl <dpjudas@users.noreply.github.com>
2016-09-11 11:09:40 +0200

    Fix random texture sampling bug

--- src/gl/renderer/gl_postprocess.cpp
+++ src/gl/renderer/gl_postprocess.cpp
@@ -191,6 +191,8 @@ void FGLRenderer::AmbientOccludeScene()
 	glBindTexture(GL_TEXTURE_2D, mBuffers->AmbientRandomTexture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glActiveTexture(GL_TEXTURE0);
 	mSSAOShader->Bind();
 	mSSAOShader->DepthTexture.Set(0);

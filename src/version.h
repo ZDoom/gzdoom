b85e3b56e38e5c1a7edfbd0640889a0dbc6b0216
raa-eruanna <eruanna@drdteam.org>
2016-09-14 02:21:35 -0400

    Establish QZDoom

--- src/version.h
+++ src/version.h
@@ -85,12 +85,12 @@ const char *GetVersionString();
 #define DYNLIGHT
 
 // This is so that derivates can use the same savegame versions without worrying about engine compatibility
-#define GAMESIG "GZDOOM"
-#define BASEWAD "gzdoom.pk3"
+#define GAMESIG "QZDOOM"
+#define BASEWAD "qzdoom.pk3"
 
 // More stuff that needs to be different for derivatives.
-#define GAMENAME "GZDoom"
-#define GAMENAMELOWERCASE "gzdoom"
+#define GAMENAME "QZDoom"
+#define GAMENAMELOWERCASE "qzdoom"
 #define FORUM_URL "http://forum.drdteam.org"
 #define BUGS_FORUM_URL	"http://forum.drdteam.org/viewforum.php?f=24"
 

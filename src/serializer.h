d5b771afdd88432175d6f30409209e1d38020e50
Christoph Oelckers <c.oelckers@zdoom.fake>
2016-09-22 11:53:09 +0200

    - fixed: Both array serializers need the bounds check.

--- src/serializer.h
+++ src/serializer.h
@@ -134,6 +134,11 @@ public:
 		}
 		if (BeginArray(key))
 		{
+			if (isReading())
+			{
+				int max = ArraySize();
+				if (max < count) count = max;
+			}
 			for (int i = 0; i < count; i++)
 			{
 				Serialize(*this, nullptr, obj[i], def ? &def[i] : nullptr);

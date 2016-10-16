59576e6d23f0d3a47c6397b970bb34f41a047416
Christoph Oelckers <coelckers@zdoom.fake>
2016-10-16 09:12:43 +0200

    - use the newly added OF_Transient flag to avoid writing out the owned dynamic lights to savegames instead of destroying and recreating them each time.

--- src/gl/scene/gl_scene.cpp
+++ src/gl/scene/gl_scene.cpp
@@ -1199,7 +1199,6 @@ void FGLInterface::StateChanged(AActor *actor)
 
 void FGLInterface::StartSerialize(FSerializer &arc)
 {
-	gl_DeleteAllAttachedLights();
 	if (arc.BeginObject("glinfo"))
 	{
 		arc("fogdensity", fogdensity)
@@ -1211,8 +1210,11 @@ void FGLInterface::StartSerialize(FSerializer &arc)
 
 void FGLInterface::EndSerialize(FSerializer &arc)
 {
-	gl_RecreateAllAttachedLights();
-	if (arc.isReading()) gl_InitPortals();
+	if (arc.isReading())
+	{
+		gl_RecreateAllAttachedLights();
+		gl_InitPortals();
+	}
 }
 
 //===========================================================================

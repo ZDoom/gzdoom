79b14bbc0854f9f813e6478b8db6f3b4db737154
raa-eruanna <eruanna@drdteam.org>
2016-10-18 11:38:35 -0400

    - Updated version.h for beta tag

--- src/version.h
+++ src/version.h
@@ -41,12 +41,12 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "0.0pre"
+#define VERSIONSTR "0.1beta"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 0,0,9999,0
-#define RC_PRODUCTVERSION 0,0,9999,0
-#define RC_PRODUCTVERSION2 "0.0pre"
+#define RC_FILEVERSION 0,1,0,0
+#define RC_PRODUCTVERSION 0,1,0,0
+#define RC_PRODUCTVERSION2 "0.1beta"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you

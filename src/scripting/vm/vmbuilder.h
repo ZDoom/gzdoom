c9dad704086ac9880ab207b90634107bddd7da52
Christoph Oelckers <c.oelckers@zdoom.fake>
2016-11-03 16:46:55 +0100

    - removed the GetConstantInt overloads since it appears they are not needed.

--- src/scripting/vm/vmbuilder.h
+++ src/scripting/vm/vmbuilder.h
@@ -30,8 +30,6 @@ public:
 
 	// Returns the constant register holding the value.
 	int GetConstantInt(int val);
-	int GetConstantInt(size_t val) { return GetConstantInt(int(val)); }
-	int GetConstantInt(unsigned val) { return GetConstantInt(int(val)); }
 	int GetConstantFloat(double val);
 	int GetConstantAddress(void *ptr, VM_ATAG tag);
 	int GetConstantString(FString str);

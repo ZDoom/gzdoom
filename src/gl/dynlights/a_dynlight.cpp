5dd63a86cf2b585972fd994fa6cc9eac5ac9aa01
Christoph Oelckers <coelckers@zdoom.fake>
2016-12-03 18:59:28 +0100

    - removed excess arguments for IMPLEMENT_CLASS.

--- src/gl/dynlights/a_dynlight.cpp
+++ src/gl/dynlights/a_dynlight.cpp
@@ -106,10 +106,10 @@ DEFINE_CLASS_PROPERTY(type, S, DynamicLight)
 // which is controlled by flags
 //
 //==========================================================================
-IMPLEMENT_CLASS (ADynamicLight, false, false, false, false)
-IMPLEMENT_CLASS (AVavoomLight, false, false, false, false)
-IMPLEMENT_CLASS (AVavoomLightWhite, false, false, false, false)
-IMPLEMENT_CLASS (AVavoomLightColor, false, false, false, false)
+IMPLEMENT_CLASS (ADynamicLight, false, false)
+IMPLEMENT_CLASS (AVavoomLight, false, false)
+IMPLEMENT_CLASS (AVavoomLightWhite, false, false)
+IMPLEMENT_CLASS (AVavoomLightColor, false, false)
 
 void AVavoomLight::BeginPlay ()
 {

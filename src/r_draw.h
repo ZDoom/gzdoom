28f9216e7bdc58473ae34ce015b74087f8604bf7
Christoph Oelckers <c.oelckers@users.noreply.github.com>
2016-12-04 16:57:10 +0100

    - fixed a few prototypes.

--- src/r_draw.h
+++ src/r_draw.h
@@ -252,10 +252,8 @@ void	R_DrawColumnHorizP_C(void);
 #ifdef X86_ASM
 
 extern "C" void	R_DrawColumnP_Unrolled (void);
-extern "C" void	R_DrawColumnHorizP_ASM (void);
 extern "C" void	R_DrawColumnP_ASM (void);
 extern "C" void	R_DrawFuzzColumnP_ASM (void);
-		   void R_DrawTranslatedColumnP_C (void);
 		   void R_DrawShadedColumnP_C (void);
 extern "C" void	R_DrawSpanP_ASM (void);
 extern "C" void R_DrawSpanMaskedP_ASM (void);
@@ -264,13 +262,14 @@ extern "C" void R_DrawSpanMaskedP_ASM (void);
 
 void	R_DrawColumnP_C (void);
 void	R_DrawFuzzColumnP_C (void);
-void	R_DrawTranslatedColumnP_C (void);
 void	R_DrawShadedColumnP_C (void);
 void	R_DrawSpanP_C (void);
 void	R_DrawSpanMaskedP_C (void);
 
 #endif
 
+void	R_DrawColumnHorizP_C(void);
+void	R_DrawTranslatedColumnP_C(void);
 void	R_DrawSpanTranslucentP_C (void);
 void	R_DrawSpanMaskedTranslucentP_C (void);
 

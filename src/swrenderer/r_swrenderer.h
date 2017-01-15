aaee6e333f291719eb0022e91605f5951ba7659d
Magnus Norddahl <dpjudas@users.noreply.github.com>
2017-01-15 02:50:37 +0100

    Mark PrecacheTexture private and remove unneeded declarations

--- src/swrenderer/r_swrenderer.h
+++ src/swrenderer/r_swrenderer.h
@@ -9,28 +9,27 @@ struct FSoftwareRenderer : public FRenderer
 	~FSoftwareRenderer();
 
 	// Can be overridden so that the colormaps for sector color/fade won't be built.
-	virtual bool UsesColormap() const override;
+	bool UsesColormap() const override;
 
-	// precache one texture
-	void PrecacheTexture(FTexture *tex, int cache);
-	virtual void Precache(BYTE *texhitlist, TMap<PClassActor*, bool> &actorhitlist) override;
+	// precache textures
+	void Precache(BYTE *texhitlist, TMap<PClassActor*, bool> &actorhitlist) override;
 
 	// render 3D view
-	virtual void RenderView(player_t *player) override;
+	void RenderView(player_t *player) override;
 
 	// Remap voxel palette
-	virtual void RemapVoxels() override;
+	void RemapVoxels() override;
 
 	// renders view to a savegame picture
-	virtual void WriteSavePic (player_t *player, FileWriter *file, int width, int height) override;
+	void WriteSavePic (player_t *player, FileWriter *file, int width, int height) override;
 
 	// draws player sprites with hardware acceleration (only useful for software rendering)
-	virtual void DrawRemainingPlayerSprites() override;
+	void DrawRemainingPlayerSprites() override;
 
-	virtual int GetMaxViewPitch(bool down) override;
+	int GetMaxViewPitch(bool down) override;
 	bool RequireGLNodes() override;
 
-	void OnModeSet () override;
+	void OnModeSet() override;
 	void ClearBuffer(int color) override;
 	void Init() override;
 	void RenderTextureView (FCanvasTexture *tex, AActor *viewpoint, int fov) override;
@@ -40,4 +39,6 @@ struct FSoftwareRenderer : public FRenderer
 	void PreprocessLevel() override;
 	void CleanLevelData() override;
 
+private:
+	void PrecacheTexture(FTexture *tex, int cache);
 };

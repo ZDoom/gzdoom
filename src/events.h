302af61686c0ae5900a0ba06d87e353c88d06aec
ZZYZX <zzyzx@virtual>
2017-01-22 07:04:35 +0200

    Added per-thing render hooks

--- src/events.h
+++ src/events.h
@@ -3,6 +3,26 @@
 
 #include "dobject.h"
 
+class DEventHandler;
+
+// register
+void E_RegisterHandler(DEventHandler* handler);
+// unregister
+void E_UnregisterHandler(DEventHandler* handler);
+
+// called right after the map has loaded (approximately same time as OPEN ACS scripts)
+void E_MapLoaded();
+// called when the map is about to unload (approximately same time as UNLOADING ACS scripts)
+void E_MapUnloading();
+// called on each render frame once.
+void E_RenderFrame();
+// called before entering each actor's view (including RenderFrame)
+void E_RenderCamera();
+// called before adding each actor to the render list
+void E_RenderBeforeThing(AActor* thing);
+// called after adding each actor to the render list
+void E_RenderAfterThing(AActor* thing);
+
 class DEventHandler : public DObject // make it a part of normal GC process
 {
 	DECLARE_CLASS(DEventHandler, DObject)
@@ -23,6 +43,10 @@ public:
 	virtual void RenderFrame();
 	// called before entering each actor's view (including RenderFrame)
 	virtual void RenderCamera();
+	// called before adding each actor to the render list
+	virtual void RenderBeforeThing(AActor* thing);
+	// called after adding each actor to the render list
+	virtual void RenderAfterThing(AActor* thing);
 };
 extern DEventHandler* E_FirstEventHandler;
 
@@ -30,31 +54,42 @@ class DRenderEventHandler : public DEventHandler
 {
 	DECLARE_CLASS(DRenderEventHandler, DEventHandler)
 public:
+	// these are for all render events
 	DVector3 ViewPos;
 	DAngle ViewAngle;
 	DAngle ViewPitch;
 	DAngle ViewRoll;
 	double FracTic; // 0..1 value that describes where we are inside the current gametic, render-wise.
+	// this makes sense in RenderCamera
+	AActor* Camera;
+	// this is for RenderBeforeThing and RenderAfterThing
+	AActor* CurrentThing;
 
 	void RenderFrame() override;
 	void RenderCamera() override;
+	void RenderBeforeThing(AActor* thing) override;
+	void RenderAfterThing(AActor* thing) override;
+
+	// this is a class that I use to automatically call RenderAfterThing.
+	// C++ is really horrible for not providing try-finally statement.
+	struct AutoThing
+	{
+		AActor* thing;
+		
+		AutoThing(AActor* thing)
+		{
+			this->thing = thing;
+			E_RenderBeforeThing(this->thing);
+		}
+
+		~AutoThing()
+		{
+			E_RenderAfterThing(this->thing);
+		}
+	};
 
 private:
 	void Setup();
 };
-
-// register
-void E_RegisterHandler(DEventHandler* handler);
-// unregister
-void E_UnregisterHandler(DEventHandler* handler);
-
-// called right after the map has loaded (approximately same time as OPEN ACS scripts)
-void E_MapLoaded();
-// called when the map is about to unload (approximately same time as UNLOADING ACS scripts)
-void E_MapUnloading();
-// called on each render frame once.
-void E_RenderFrame();
-// called before entering each actor's view (including RenderFrame)
-void E_RenderCamera();
 
 #endif

a226337185b67f18d3e1b1a95b492f8ab92d7467
ZZYZX <zzyzx@virtual>
2017-01-22 07:50:18 +0200

    More portal fixing in software renderer

--- src/r_main.cpp
+++ src/r_main.cpp
@@ -618,9 +618,6 @@ void R_HighlightPortal (PortalDrawseg* pds)
 
 void R_EnterPortal (PortalDrawseg* pds, int depth)
 {
-	// [ZZ] portal hook
-	E_RenderCamera();
-
 	// [ZZ] check depth. fill portal with black if it's exceeding the visual recursion limit, and continue like nothing happened.
 	if (depth >= r_portal_recursions)
 	{
@@ -722,6 +719,9 @@ void R_EnterPortal (PortalDrawseg* pds, int depth)
 	ViewTanCos = FocalTangent * ViewCos;
 
 	R_CopyStackedViewParameters();
+
+	// [ZZ] portal hook
+	E_RenderCamera();
 
 	validcount++;
 	PortalDrawseg* prevpds = CurrentPortal;

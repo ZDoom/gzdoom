6c9558459d941a072ffd3fb4f281200be24c5a68
Christoph Oelckers <coelckers@users.noreply.github.com>
2017-02-19 18:15:40 +0100

    - allow menu items to inherit their parent's Init method for MENUDEFS.

--- src/menu/menudef.cpp
+++ src/menu/menudef.cpp
@@ -369,7 +369,7 @@ static void ParseListMenuBody(FScanner &sc, DListMenuDescriptor *desc)
 			PClass *cls = PClass::FindClass(buildname);
 			if (cls != nullptr && cls->IsDescendantOf("ListMenuItem"))
 			{
-				auto func = dyn_cast<PFunction>(cls->Symbols.FindSymbol("Init", false));
+				auto func = dyn_cast<PFunction>(cls->Symbols.FindSymbol("Init", true));
 				if (func != nullptr && !(func->Variants[0].Flags & (VARF_Protected | VARF_Private)))	// skip internal classes which have a protexted init method.
 				{
 					auto &args = func->Variants[0].Proto->ArgumentTypes;
@@ -741,7 +741,7 @@ static void ParseOptionMenuBody(FScanner &sc, DOptionMenuDescriptor *desc)
 			PClass *cls = PClass::FindClass(buildname);
 			if (cls != nullptr && cls->IsDescendantOf("OptionMenuItem"))
 			{
-				auto func = dyn_cast<PFunction>(cls->Symbols.FindSymbol("Init", false));
+				auto func = dyn_cast<PFunction>(cls->Symbols.FindSymbol("Init", true));
 				if (func != nullptr && !(func->Variants[0].Flags & (VARF_Protected | VARF_Private)))	// skip internal classes which have a protexted init method.
 				{
 					auto &args = func->Variants[0].Proto->ArgumentTypes;

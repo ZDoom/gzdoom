1a9b1de9a1f855102bd00250d8abb9d6a7ab6300
Rachael Alexanderson <eruanna@drdteam.org>
2017-02-23 03:39:04 -0500

    - Added "clipmidtex" compatflag and applied it to unloved.pk3

--- src/compatibility.cpp
+++ src/compatibility.cpp
@@ -151,7 +151,7 @@ static FCompatOption Options[] =
 	{ "multiexit",				COMPATF2_MULTIEXIT, SLOT_COMPAT2 },
 	{ "teleport",				COMPATF2_TELEPORT, SLOT_COMPAT2 },
 	{ "disablepushwindowcheck",	COMPATF2_PUSHWINDOW, SLOT_COMPAT2 },
-
+	{ "clipmidtex",				COMPATF2_CLIPMIDTEX, SLOT_COMPAT2 },
 	{ NULL, 0, 0 }
 };
 
--- src/doomdef.h
+++ src/doomdef.h
@@ -344,6 +344,7 @@ enum : unsigned int
 	COMPATF2_MULTIEXIT		= 1 << 4,	// Level exit can be triggered multiple times (required by Daedalus's travel tubes, thanks to a faulty script)
 	COMPATF2_TELEPORT		= 1 << 5,	// Don't let indirect teleports trigger sector actions
 	COMPATF2_PUSHWINDOW		= 1 << 6,	// Disable the window check in CheckForPushSpecial()
+	COMPATF2_CLIPMIDTEX		= 1 << 7,	// Always Clip midtex's in the software renderer (required to run certain GZDoom maps)
 };
 
 // Emulate old bugs for select maps. These are not exposed by a cvar

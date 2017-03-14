959fb2577bdd8187c26bae8213ccf4d80f5f07e1
Rachael Alexanderson <eruanna@drdteam.org>
2017-03-14 04:02:40 -0400

    - Unfriendly players now spawn at deathmatch starts.

--- src/g_game.cpp
+++ src/g_game.cpp
@@ -1766,6 +1766,8 @@ void G_DoReborn (int playernum, bool freshbot)
 	}
 	else
 	{
+		bool isUnfriendly = players[playernum].mo && !(players[playernum].mo->flags & MF_FRIENDLY);
+
 		// respawn at the start
 		// first disassociate the corpse
 		if (players[playernum].mo)
@@ -1775,7 +1777,7 @@ void G_DoReborn (int playernum, bool freshbot)
 		}
 
 		// spawn at random spot if in deathmatch
-		if (deathmatch)
+		if (deathmatch || isUnfriendly)
 		{
 			G_DeathMatchSpawnPlayer (playernum);
 			return;

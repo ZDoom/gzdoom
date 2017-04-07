e690de25a89298b5c3bfb12689e3265d5625c830
Magnus Norddahl <dpjudas@users.noreply.github.com>
2017-04-07 02:17:34 +0200

    - Use SDL_GL_GetProcAddress on platforms where the SDL is being used. Since SDL initialized OpenGL for us, it is the only reliable way of retrieving proc addresses. - Check if ogl_LoadFunctions failed and make OpenGLSWFrameBuffer gracefully recover from that

--- src/gl/system/gl_load.c
+++ src/gl/system/gl_load.c
@@ -85,7 +85,9 @@ static PROC WinGetProcAddress(const char *name)
 		#define IntGetProcAddress(name) AppleGLGetProcAddress(name)
 	#else
 		#if defined(__sgi) || defined(__sun) || defined(__unix__)
-			#define IntGetProcAddress(name) PosixGetProcAddress((const GLubyte*)name)
+			void* SDL_GL_GetProcAddress(const char* proc);
+			#define IntGetProcAddress(name) SDL_GL_GetProcAddress((const char*)name)
+			//#define IntGetProcAddress(name) PosixGetProcAddress((const GLubyte*)name)
 /* END OF MANUAL CHANGES, DO NOT REMOVE! */
 		#else /* GLX */
 		    #include <GL/glx.h>

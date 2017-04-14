d03c33f39330121a7e42c1d70c750a4d94410f65
Christoph Oelckers <coelckers@users.noreply.github.com>
2017-04-14 17:40:05 +0200

    - added Translation.GetID.

--- src/r_data/r_translate.cpp
+++ src/r_data/r_translate.cpp
@@ -1356,6 +1356,13 @@ int R_FindCustomTranslation(FName name)
 	return (t != nullptr)? *t : -1;
 }
 
+DEFINE_ACTION_FUNCTION(_Translation, GetID)
+{
+	PARAM_PROLOGUE;
+	PARAM_NAME(t);
+	ACTION_RETURN_INT(R_FindCustomTranslation(t));
+}
+
 //----------------------------------------------------------------------------
 //
 //

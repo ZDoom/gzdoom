b8074a4e53f26467eadda8591e4ef0f2edca2dcc
Rachael Alexanderson <eruanna@drdteam.org>
2017-05-31 09:15:13 -0400

    - Sync VER_MAJOR and VER_MINOR in version.h with GZDoom parent.

--- src/version.h
+++ src/version.h
@@ -55,9 +55,9 @@ const char *GetVersionString();
 #define RC_FILEVERSION 1,3,9999,0
 #define RC_PRODUCTVERSION 1,3,9999,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
-// These are for content versioning. The current state is '2.4'.
-#define VER_MAJOR 2
-#define VER_MINOR 5
+// These are for content versioning. The current state is '3.2'.
+#define VER_MAJOR 3
+#define VER_MINOR 2
 #define VER_REVISION 0
 
 // Version identifier for network games.

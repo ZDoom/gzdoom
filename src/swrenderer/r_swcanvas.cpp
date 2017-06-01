5502421901c6e25d186ae5dc4a81b5d3554c1a84
Rachael Alexanderson <eruanna@drdteam.org>
2017-06-01 16:21:15 -0400

    - fixed: Using pure software drawers (vid_hw2d==false) drawing stencils on the screen resulted in black stencils - this fixes crosshairs being drawn black

--- src/swrenderer/r_swcanvas.cpp
+++ src/swrenderer/r_swcanvas.cpp
@@ -105,7 +105,8 @@ void SWCanvas::DrawTexture(DCanvas *canvas, FTexture *img, DrawParms &parms)
 	drawerargs.SetTranslationMap(translation);
 	drawerargs.SetLight(basecolormap, 0.0f, shade);
 
-	bool visible = drawerargs.SetStyle(viewport, parms.style, parms.Alpha, -1, parms.fillcolor, basecolormap);
+	uint32_t myfillcolor = (RGB256k.All[((parms.fillcolor & 0xfc0000) >> 6) | ((parms.fillcolor & 0xfc00) >> 4) | ((parms.fillcolor & 0xfc) >> 2)]) << 24;
+	bool visible = drawerargs.SetStyle(viewport, parms.style, parms.Alpha, -1, myfillcolor, basecolormap);
 
 	double x0 = parms.x - parms.left * parms.destwidth / parms.texwidth;
 	double y0 = parms.y - parms.top * parms.destheight / parms.texheight;

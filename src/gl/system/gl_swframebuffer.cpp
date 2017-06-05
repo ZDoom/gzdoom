3ee6c8abc18ce6adab55933c0c57b529ba67f22d
svdijk <svdijk@users.sourceforge.net>
2017-06-05 11:41:54 +0200

    Fix setting of the gl_legacy_mode cvar.

--- src/gl/system/gl_swframebuffer.cpp
+++ src/gl/system/gl_swframebuffer.cpp
@@ -207,12 +207,15 @@ OpenGLSWFrameBuffer::OpenGLSWFrameBuffer(void *hMonitor, int width, int height, 
 	const char *glversion = (const char*)glGetString(GL_VERSION);
 	bool isGLES = (glversion && strlen(glversion) > 10 && memcmp(glversion, "OpenGL ES ", 10) == 0);
 
+	UCVarValue value;
 	// GL 3.0 is mostly broken on MESA drivers which really are the only relevant case here that doesn't fulfill the requirements based on version number alone.
 #ifdef _WIN32
-	gl_legacy_mode = !ogl_IsVersionGEQ(3, 0);
+	value.Bool = !ogl_IsVersionGEQ(3, 0);
 #else
-	gl_legacy_mode = !ogl_IsVersionGEQ(3, 1);
+	value.Bool = !ogl_IsVersionGEQ(3, 1);
 #endif
+	gl_legacy_mode.ForceSet (value, CVAR_Bool);
+
 	if (!isGLES && ogl_IsVersionGEQ(3, 0) == 0)
 	{
 		Printf("OpenGL acceleration requires at least OpenGL 3.0. No Acceleration will be used.\n");

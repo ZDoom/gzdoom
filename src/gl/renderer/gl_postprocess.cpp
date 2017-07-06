a4b96f767397d01616092b1cfeb24d888d55da84
Magnus Norddahl <dpjudas@users.noreply.github.com>
2017-07-06 18:02:27 +0200

    - Fix linker error

--- src/gl/renderer/gl_postprocess.cpp
+++ src/gl/renderer/gl_postprocess.cpp
@@ -148,8 +148,6 @@ CUSTOM_CVAR(Bool, gl_paltonemap_reverselookup, true, CVAR_ARCHIVE | CVAR_NOINITC
 
 CVAR(Float, gl_menu_blur, -1.0f, CVAR_ARCHIVE)
 
-CVAR(Bool, gl_custompost, true, 0)
-
 EXTERN_CVAR(Float, vid_brightness)
 EXTERN_CVAR(Float, vid_contrast)
 EXTERN_CVAR(Float, vid_saturation)

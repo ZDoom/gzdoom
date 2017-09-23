f174111128465805d122f2e5167c5e54a42bfd13
alexey.lysiuk <alexey.lysiuk@gmail.com>
2017-09-23 11:57:06 +0300

    Fixed read beyond buffer boundary during font color parsing
    Printing of string that ends with '\c' led to undefined behavior
    Example: 'echo \c' in console

--- src/v_font.cpp
+++ src/v_font.cpp
@@ -2666,7 +2666,7 @@ EColorRange V_ParseFontColor (const uint8_t *&color_value, int normalcolor, int 
 	}
 	else							// Incomplete!
 	{
-		color_value = ch - (*ch == '\0');
+		color_value = ch - (newcolor == '\0');
 		return CR_UNDEFINED;
 	}
 	color_value = ch;

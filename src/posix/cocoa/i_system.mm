7d63c3324d391090f59f6c97efa5f794dbfa94ca
Magnus Norddahl <dpjudas@users.noreply.github.com>
2017-11-12 03:51:37 +0100

    - Fix macOS compile errors

--- src/posix/cocoa/i_system.mm
+++ src/posix/cocoa/i_system.mm
@@ -73,18 +73,6 @@ ticcmd_t* I_BaseTiccmd()
 }
 
 
-void I_WaitVBL(const int count)
-{
-    // I_WaitVBL is never used to actually synchronize to the
-    // vertical blank. Instead, it's used for delay purposes.
-    struct timespec delay, rem;
-    delay.tv_sec = count / 70;
-    /* Avoid overflow. Microsec res should be good enough. */
-    delay.tv_nsec = (count%70)*1000000/70 * 1000;
-    while(nanosleep(&delay, &rem) == -1 && errno == EINTR)
-        delay = rem;
-}
-
 
 //
 // SetLanguageIDs

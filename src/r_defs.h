f629678320d22f70d617f09ba720726f7ba29970
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-01-18 15:34:56 +0200

    Fixed const correctness for secplane structure
    https://forum.zdoom.org/viewtopic.php?t=59151

--- src/r_defs.h
+++ src/r_defs.h
@@ -394,7 +394,7 @@ public:
 	}
 
 	// Moves a plane up/down by hdiff units
-	double GetChangedHeight(double hdiff)
+	double GetChangedHeight(double hdiff) const
 	{
 		return D - hdiff * normal.Z;
 	}

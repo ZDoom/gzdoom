55c6a14059717992a04a9e21ee637d0f24ac5292
Alexander Wilms <f.alexander.wilms@gmail.com>
2018-01-24 05:31:48 +0100

    Added simple fatal error window for Linux
    There are three variants: KDE dialog, GTK+ window, SDL message box
    https://forum.zdoom.org/viewtopic.php?t=57880

--- src/posix/sdl/i_main.cpp
+++ src/posix/sdl/i_main.cpp
@@ -73,6 +73,10 @@ extern "C" int cc_install_handlers(int, char**, int, int*, const char*, int(*)(c
 void Mac_I_FatalError(const char* errortext);
 #endif
 
+#ifdef __linux__
+void Linux_I_FatalError(const char* errortext);
+#endif
+
 // PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
 
 // PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
@@ -267,6 +271,10 @@ int main (int argc, char **argv)
 #ifdef __APPLE__
 		Mac_I_FatalError(error.GetMessage());
 #endif // __APPLE__
+
+#ifdef __linux__
+		Linux_I_FatalError(error.GetMessage());
+#endif // __linux__
 
 		exit (-1);
     }

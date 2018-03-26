d166ab95a99ed4807e03daccfa68780b15189b92
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-03-26 19:44:41 +0200

    Revert "- fixed: 3D floor that extend into the real sector's floor were not clipped properly."
    This reverts commit a33ad3c99eff7fcdc99967aee72b0ead5027e4c4.
    
    Turns out that this breaks legitimate maps. So I'd rather let that one broken map glitch than the good ones.

--- src/gl/scene/gl_walls.cpp
+++ src/gl/scene/gl_walls.cpp
@@ -1392,12 +1392,6 @@ void GLWall::DoFFloorBlocks(seg_t * seg, sector_t * frontsector, sector_t * back
 			ff_topleft = topleft;
 			ff_topright = topright;
 		}
-		if (ff_bottomleft < bottomleft && ff_bottomright < bottomright)
-		{
-			// the new section extends into the floor.
-			ff_bottomleft = bottomleft;
-			ff_bottomright = bottomright;
-		}
 
 		// do all inverse floors above the current one it there is a gap between the
 		// last 3D floor and this one.

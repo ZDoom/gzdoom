f8019637cf95d51f3bf4c19532944e93f17d20c3
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-03-26 11:45:53 +0200

    - fixed: FBuildTexture::CopyTrueColorPixels returned incorrect transparency information

--- src/textures/buildtexture.cpp
+++ src/textures/buildtexture.cpp
@@ -104,7 +104,7 @@ int FBuildTexture::CopyTrueColorPixels(FBitmap *bmp, int x, int y, int rotate, F
 {
 	PalEntry *Remap = translationtables[TRANSLATION_Standard][Translation]->Palette;
 	bmp->CopyPixelData(x, y, RawPixels, Width, Height, Height, 1, rotate, Remap, inf);
-	return 0;
+	return -1;
 
 }
 

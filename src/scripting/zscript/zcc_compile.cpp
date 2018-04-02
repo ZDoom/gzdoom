ac7e5def3252291a3a8e95651edc63bf62c523a6
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-04-02 15:18:30 +0300

    Forbade dynamic array as the return type of a function
    Compiler ignored this case silently but it crashed during code generation

--- src/scripting/zscript/zcc_compile.cpp
+++ src/scripting/zscript/zcc_compile.cpp
@@ -2316,6 +2316,11 @@ void ZCCCompiler::CompileFunction(ZCC_StructWork *c, ZCC_FuncDeclarator *f, bool
 					// structs and classes only get passed by pointer.
 					type = NewPointer(type);
 				}
+				else if (type->isDynArray())
+				{
+					Error(f, "The return type of a function cannot be a dynamic array");
+					break;
+				}
 				// TBD: disallow certain types? For now, let everything pass that isn't an array.
 				rets.Push(type);
 				t = static_cast<decltype(t)>(t->SiblingNext);

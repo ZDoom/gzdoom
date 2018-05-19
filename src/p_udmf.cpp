90cb0b32158c37f7893009f54ef348b2d1acad13
RockstarRaccoon <raccoon@raccoon-dev.com>
2018-05-19 09:31:22 +0300

    - added 'revealed on automap' linedef flag, UDMF only
    https://forum.zdoom.org/viewtopic.php?t=59808

--- src/p_udmf.cpp
+++ src/p_udmf.cpp
@@ -1107,6 +1107,10 @@ public:
 				Flag(ld->flags, ML_3DMIDTEX_IMPASS, key);
 				continue;
 
+			case NAME_Revealed:
+				Flag(ld->flags, ML_REVEALED, key);
+				continue;
+
 			case NAME_MoreIds:
 				// delay parsing of the tag string until parsing of the sector is complete
 				// This ensures that the ID is always the first tag in the list.

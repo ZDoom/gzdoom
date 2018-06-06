d9b6a8bd139082bd5b4797807a329e6326139349
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-06-06 23:23:57 +0200

    - This is version 3.4.0.

--- src/version.h
+++ src/version.h
@@ -48,12 +48,12 @@ const char *GetVersionString();
 #ifdef GIT_DESCRIPTION
 #define VERSIONSTR GIT_DESCRIPTION
 #else
-#define VERSIONSTR "3.3pre"
+#define VERSIONSTR "g3.4.0"
 #endif
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 3,2,9999,0
-#define RC_PRODUCTVERSION 3,2,9999,0
+#define RC_FILEVERSION 3,4,0,0
+#define RC_PRODUCTVERSION 3,4,0,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning. The current state is '3.3'.
 #define VER_MAJOR 3

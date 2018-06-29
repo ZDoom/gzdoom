c30505d02aa9f8b5d961f748e4b8870f35a167b2
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-06-29 11:33:59 +0300

    - fixed excess keyboard events in Cocoa backend
    https://forum.zdoom.org/viewtopic.php?t=61104

--- src/posix/cocoa/i_input.mm
+++ src/posix/cocoa/i_input.mm
@@ -559,11 +559,13 @@ void ProcessKeyboardEvent(NSEvent* theEvent)
 		return;
 	}
 
+	const bool isARepeat = [theEvent isARepeat];
+
 	if (k_allowfullscreentoggle
 		&& (kVK_ANSI_F == keyCode)
 		&& (NSCommandKeyMask & [theEvent modifierFlags])
 		&& (NSKeyDown == [theEvent type])
-		&& ![theEvent isARepeat])
+		&& !isARepeat)
 	{
 		ToggleFullscreen = !ToggleFullscreen;
 		return;
@@ -573,7 +575,7 @@ void ProcessKeyboardEvent(NSEvent* theEvent)
 	{
 		ProcessKeyboardEventInMenu(theEvent);
 	}
-	else
+	else if (!isARepeat)
 	{
 		event_t event = {};
 

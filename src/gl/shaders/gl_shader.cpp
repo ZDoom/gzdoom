fa51a54042bb29d9f36b5de62275eb31a0734eb9
usernameak <delta.cxx@gmail.com+>
2018-07-05 15:01:16 +0300

    Made ability to define custom material shaders for ProcessMaterial function

--- src/gl/shaders/gl_shader.cpp
+++ src/gl/shaders/gl_shader.cpp
@@ -496,6 +496,7 @@ static const FDefaultShader defaultshaders[]=
 };
 
 TArray<FString> usershaders;
+TArray<FString> usermaterials;
 
 struct FEffectShader
 {
@@ -647,7 +648,7 @@ void FShaderCollection::CompileShaders(EPassType passType)
 		FString name = ExtractFileBase(usershaders[i]);
 		FName sfn = name;
 
-		FShader *shc = Compile(sfn, usershaders[i], "shaders/glsl/material_normal.fp", "", true, passType);
+		FShader *shc = Compile(sfn, usershaders[i], usermaterials[i], "", true, passType);
 		mMaterialShaders.Push(shc);
 	}
 

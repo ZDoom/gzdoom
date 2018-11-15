47138b748ae15b9d7b9b7d2df7f3bf06795aef96
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-11-15 16:53:19 +0100

    Make BounceFlags 32 bit wide.

--- src/actor.h
+++ src/actor.h
@@ -551,7 +551,7 @@ typedef TFlags<ActorFlag6> ActorFlags6;
 typedef TFlags<ActorFlag7> ActorFlags7;
 typedef TFlags<ActorFlag8> ActorFlags8;
 typedef TFlags<ActorRenderFlag> ActorRenderFlags;
-typedef TFlags<ActorBounceFlag, uint16_t> ActorBounceFlags;
+typedef TFlags<ActorBounceFlag> ActorBounceFlags;
 typedef TFlags<ActorRenderFeatureFlag> ActorRenderFeatureFlags;
 DEFINE_TFLAGS_OPERATORS (ActorFlags)
 DEFINE_TFLAGS_OPERATORS (ActorFlags2)

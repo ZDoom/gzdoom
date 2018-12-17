1ef5becd4f9ed619b9a2e7493f7a946756700d98
drfrag <drfrag666@hotmail.com>
2018-12-17 22:48:09 +0100

    - Fixed compilation with MinGW-w64 64 bit.

--- src/scripting/vm/jit_runtime.cpp
+++ src/scripting/vm/jit_runtime.cpp
@@ -291,7 +291,11 @@ void *AddJitFunction(asmjit::CodeHolder* code, JitCompiler *compiler)
 	RUNTIME_FUNCTION *table = (RUNTIME_FUNCTION*)(unwindptr + unwindInfoSize);
 	table[0].BeginAddress = (DWORD)(ptrdiff_t)(startaddr - baseaddr);
 	table[0].EndAddress = (DWORD)(ptrdiff_t)(endaddr - baseaddr);
+#ifndef __MINGW64__
 	table[0].UnwindInfoAddress = (DWORD)(ptrdiff_t)(unwindptr - baseaddr);
+#else
+	table[0].UnwindData = (DWORD)(ptrdiff_t)(unwindptr - baseaddr);
+#endif
 	BOOLEAN result = RtlAddFunctionTable(table, 1, (DWORD64)baseaddr);
 	JitFrames.Push((uint8_t*)table);
 	if (result == 0)

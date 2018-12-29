f35ac75ca2db47ef759ebe4b5e57f3492f7494b5
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-12-29 14:01:07 +0100

    - fixed precaching of switches.
    The backwards animation accessed the wrong array which in case of sequences with different length could crash

--- src/p_setup.cpp
+++ src/p_setup.cpp
@@ -113,7 +113,7 @@ static void AddToList(uint8_t *hitlist, FTextureID texid, int bitmask)
 		}
 		for (int i = 0; i < switchdef->PairDef->NumFrames; i++)
 		{
-			hitlist[switchdef->frames[i].Texture.GetIndex()] |= (uint8_t)bitmask;
+			hitlist[switchdef->PairDef->frames[i].Texture.GetIndex()] |= (uint8_t)bitmask;
 		}
 	}
 

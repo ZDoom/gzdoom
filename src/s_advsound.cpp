980977d444bdd0921c99a7f5cfa237d556583f70
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-01-04 16:08:45 +0100

    - fixed: GetSoundClass must be able to handle non-players.

--- src/s_advsound.cpp
+++ src/s_advsound.cpp
@@ -1906,7 +1906,7 @@ static const char *GetSoundClass(AActor *pp)
 	{
 		return Skins[player->userinfo.GetSkin()].Name.GetChars();
 	}
-	auto sclass = pp->NameVar(NAME_SoundClass);
+	auto sclass = player? pp->NameVar(NAME_SoundClass) : NAME_None;
 
 	return sclass != NAME_None ? sclass.GetChars() : "player";
 }

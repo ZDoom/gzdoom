241835bf2c61bad985de35cafd1079b7b87f32fa
Marrub <marrub--@users.noreply.github.com>
2019-01-09 08:53:10 -0500

    Fix return value of native BuiltinCallLineSpecial

--- src/scripting/backend/codegen.cpp
+++ src/scripting/backend/codegen.cpp
@@ -8530,9 +8530,9 @@ FxExpression *FxActionSpecialCall::Resolve(FCompileContext& ctx)
 //
 //==========================================================================
 
-void BuiltinCallLineSpecial(int special, AActor *activator, int arg1, int arg2, int arg3, int arg4, int arg5)
+int BuiltinCallLineSpecial(int special, AActor *activator, int arg1, int arg2, int arg3, int arg4, int arg5)
 {
-	P_ExecuteSpecial(special, nullptr, activator, 0, arg1, arg2, arg3, arg4, arg5);
+	return P_ExecuteSpecial(special, nullptr, activator, 0, arg1, arg2, arg3, arg4, arg5);
 }
 
 DEFINE_ACTION_FUNCTION_NATIVE(DObject, BuiltinCallLineSpecial, BuiltinCallLineSpecial)

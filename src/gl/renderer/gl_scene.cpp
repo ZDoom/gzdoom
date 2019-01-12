7c54c6c5294255d7fc585b9d949ac21a03e1b133
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-01-12 14:59:26 +0100

    - fixed incompatible commit.

--- src/gl/renderer/gl_scene.cpp
+++ src/gl/renderer/gl_scene.cpp
@@ -153,7 +153,7 @@ sector_t * FGLRenderer::RenderViewpoint (FRenderViewpoint &mainvp, AActor * came
 	R_SetupFrame (mainvp, r_viewwindow, camera);
 
 	if (mainview && toscreen)
-		UpdateShadowMap(camera->Level);
+		UpdateShadowMap();
 
     // Render (potentially) multiple views for stereo 3d
 	// Fixme. The view offsetting should be done with a static table and not require setup of the entire render state for the mode.

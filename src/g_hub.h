8652f140f2798ad4fee625f09ae28908627bc603
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-01-29 02:17:25 +0100

    - fixed missing declaration

--- src/g_hub.h
+++ src/g_hub.h
@@ -4,6 +4,7 @@
 struct cluster_info_t;
 struct wbstartstruct_t;
 class FSerializer;
+struct FLevelLocals;
 
 void G_SerializeHub (FSerializer &file);
 void G_LeavingHub(FLevelLocals *Level, int mode, cluster_info_t * cluster, struct wbstartstruct_t * wbs);

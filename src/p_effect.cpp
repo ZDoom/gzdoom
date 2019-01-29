14971c8c3a5ebfe42f255ef65a306b68cdf765b1
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-01-29 04:20:36 +0100

    - missed references in p_effect.cpp

--- src/p_effect.cpp
+++ src/p_effect.cpp
@@ -170,12 +170,12 @@ void P_ClearParticles (FLevelLocals *Level)
 
 void P_FindParticleSubsectors (FLevelLocals *Level)
 {
-	if (Level->ParticlesInSubsec.Size() < level.subsectors.Size())
+	if (Level->ParticlesInSubsec.Size() < Level->subsectors.Size())
 	{
-		Level->ParticlesInSubsec.Reserve (level.subsectors.Size() - Level->ParticlesInSubsec.Size());
+		Level->ParticlesInSubsec.Reserve (Level->subsectors.Size() - Level->ParticlesInSubsec.Size());
 	}
 
-	fillshort (&Level->ParticlesInSubsec[0], level.subsectors.Size(), NO_PARTICLE);
+	fillshort (&Level->ParticlesInSubsec[0], Level->subsectors.Size(), NO_PARTICLE);
 
 	if (!r_particles)
 	{
@@ -239,7 +239,7 @@ void P_ThinkParticles (FLevelLocals *Level)
 	{
 		particle = &Level->Particles[i];
 		i = particle->tnext;
-		if (!particle->notimefreeze && level.isFrozen())
+		if (!particle->notimefreeze && Level->isFrozen())
 		{
 			prev = particle;
 			continue;
@@ -326,7 +326,7 @@ void P_RunEffects (FLevelLocals *Level)
 {
 	if (players[consoleplayer].camera == NULL) return;
 
-	int	pnum = players[consoleplayer].camera->Sector->Index() * level.sectors.Size();
+	int	pnum = players[consoleplayer].camera->Sector->Index() * Level->sectors.Size();
 
 	AActor *actor;
 	TThinkerIterator<AActor> iterator;
@@ -375,7 +375,7 @@ static void MakeFountain (AActor *actor, int color1, int color2)
 {
 	particle_t *particle;
 
-	if (!(level.time & 1))
+	if (!(actor->Level->time & 1))
 		return;
 
 	particle = JitterParticle (actor->Level, 51);

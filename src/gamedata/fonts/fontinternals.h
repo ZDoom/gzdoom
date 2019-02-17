7f1f25d99898199be13b6090a2d7a25d99f5b271
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-02-17 12:00:04 +0100

    - split the FSingleLumpFont class into its own file.

---
+++ src/gamedata/fonts/fontinternals.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+#include "tarray.h"
+
+// This structure is used by BuildTranslations() to hold color information.
+struct TranslationParm
+{
+	short RangeStart;	// First level for this range
+	short RangeEnd;		// Last level for this range
+	uint8_t Start[3];		// Start color for this range
+	uint8_t End[3];		// End color for this range
+};
+
+
+extern TArray<TranslationParm> TranslationParms[2];

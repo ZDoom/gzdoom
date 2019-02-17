95e62e91bb32831888f9557d6152e7529cb3c328
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-02-17 13:50:57 +0100

    - split the FFont base class into its own file.

--- src/gamedata/fonts/fontinternals.h
+++ src/gamedata/fonts/fontinternals.h
@@ -12,7 +12,33 @@ struct TranslationParm
 	uint8_t End[3];		// End color for this range
 };
 
+struct TempParmInfo
+{
+	unsigned int StartParm[2];
+	unsigned int ParmLen[2];
+	int Index;
+};
+struct TempColorInfo
+{
+	FName Name;
+	unsigned int ParmInfo;
+	PalEntry LogColor;
+};
+
+struct TranslationMap
+{
+	FName Name;
+	int Number;
+};
 
 extern TArray<TranslationParm> TranslationParms[2];
+extern TArray<TranslationMap> TranslationLookup;
+extern TArray<PalEntry> TranslationColors;
+extern uint16_t lowerforupper[65536];
+extern uint16_t upperforlower[65536];
+
+class FImageSource;
 
 void RecordTextureColors (FImageSource *pic, uint8_t *usedcolors);
+bool myislower(int code);
+int stripaccent(int code);

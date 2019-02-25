e8f690116b8392fce86c3bdb3bc54e9a2a5f6463
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-02-26 00:07:30 +0100

    - removed unused gl_spritebrightfog CVAR.

--- src/rendering/hwrenderer/utility/hw_cvars.h
+++ src/rendering/hwrenderer/utility/hw_cvars.h
@@ -55,7 +55,6 @@ EXTERN_CVAR(Bool, gl_debug_breakpoint)
 
 
 EXTERN_CVAR(Bool, gl_usecolorblending)
-EXTERN_CVAR(Bool, gl_spritebrightfog)
 EXTERN_CVAR(Bool, gl_sprite_blend)
 EXTERN_CVAR(Int, gl_spriteclip)
 EXTERN_CVAR(Float, gl_sclipthreshold)

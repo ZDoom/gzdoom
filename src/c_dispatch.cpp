02836b3c3f6aec8216c1c5a03f8a4b6c9e50b7ee
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-03-11 00:26:37 +0100

    - fixed: The command line parser of the console was incapable of handling non-ASCII characters.

--- src/c_dispatch.cpp
+++ src/c_dispatch.cpp
@@ -563,7 +563,7 @@ void C_DoCommand (const char *cmd, int keynum)
 	const char *beg;
 
 	// Skip any beginning whitespace
-	while (*cmd && *cmd <= ' ')
+	while (*cmd > 0 && *cmd <= ' ')
 		cmd++;
 
 	// Find end of the command name
@@ -575,7 +575,7 @@ void C_DoCommand (const char *cmd, int keynum)
 	else
 	{
 		beg = cmd;
-		for (end = cmd+1; *end > ' '; ++end)
+		for (end = cmd+1; *end > ' ' || *end < 0; ++end)
 			;
 	}
 
@@ -728,7 +728,7 @@ void AddCommandString (const char *text, int keynum)
 				more = 0;
 			}
 			// Intercept wait commands here. Note: wait must be lowercase
-			while (*cmd && *cmd <= ' ')
+			while (*cmd > 0 && *cmd <= ' ')
 				cmd++;
 			if (*cmd)
 			{

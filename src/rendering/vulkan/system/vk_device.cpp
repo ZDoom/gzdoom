b40983be61b000f2570402787b898824c60e6c93
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-03-26 08:48:10 +0100

    - fixed bad Printf formatter.

--- src/rendering/vulkan/system/vk_device.cpp
+++ src/rendering/vulkan/system/vk_device.cpp
@@ -375,7 +375,7 @@ VkBool32 VulkanDevice::DebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT mess
 		if (callbackData->pObjects[i].pObjectName)
 		{
 			FString hexname;
-			hexname.Format("0x%x", callbackData->pObjects[i].objectHandle);
+			hexname.Format("0x%llx", callbackData->pObjects[i].objectHandle);
 			msg.Substitute(hexname.GetChars(), callbackData->pObjects[i].pObjectName);
 		}
 	}

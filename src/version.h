5e953294245cc641266d10297e791afa4773491b
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2019-04-28 02:37:42 -0400

    - 4.2pre

--- src/version.h
+++ src/version.h
@@ -41,15 +41,15 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.1pre"
+#define VERSIONSTR "4.2pre"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,0,9999,0
-#define RC_PRODUCTVERSION 4,0,9999,0
+#define RC_FILEVERSION 4,1,9999,0
+#define RC_PRODUCTVERSION 4,1,9999,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
-#define VER_MINOR 1
+#define VER_MINOR 2
 #define VER_REVISION 0
 
 // This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!

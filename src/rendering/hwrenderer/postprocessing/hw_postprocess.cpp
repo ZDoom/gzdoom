6a4e60ef71e47a31c160a77d77cd1896d747fc45
Magnus Norddahl <dpjudas@users.noreply.github.com>
2019-06-05 19:06:33 +0200

    - FXAA_DISCARD should never be enabled. It only works when the output framebuffer is the same as the original.

--- src/rendering/hwrenderer/postprocessing/hw_postprocess.cpp
+++ src/rendering/hwrenderer/postprocessing/hw_postprocess.cpp
@@ -389,7 +389,6 @@ FString PPFXAA::GetDefines()
 	}
 
 	const int gatherAlpha = GetMaxVersion() >= 400 ? 1 : 0;
-	const int discard = (screen->IsVulkan() && strstr(screen->vendorstring, "AMD") != nullptr) ? 0 : 1;
 
 	// TODO: enable FXAA_GATHER4_ALPHA on OpenGL earlier than 4.0
 	// when GL_ARB_gpu_shader5/GL_NV_gpu_shader5 extensions are supported
@@ -397,9 +396,8 @@ FString PPFXAA::GetDefines()
 	FString result;
 	result.Format(
 		"#define FXAA_QUALITY__PRESET %i\n"
-		"#define FXAA_GATHER4_ALPHA %i\n"
-		"#define FXAA_DISCARD %i\n",
-		quality, gatherAlpha, discard);
+		"#define FXAA_GATHER4_ALPHA %i\n",
+		quality, gatherAlpha);
 
 	return result;
 }

115d1cb182168bb2c8c29a3650dfc33b08e3e6cb
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-09-23 14:27:43 +0200

    - made the Timidity++ backend a library, now that it has no dependencies on GZDoom anymore.

--- libraries/timidityplus/common.cpp
+++ libraries/timidityplus/common.cpp
@@ -23,6 +23,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 #include <string.h>
 #include <ctype.h>
--- libraries/timidityplus/playmidi.cpp
+++ libraries/timidityplus/playmidi.cpp
@@ -22,6 +22,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <algorithm>
 
 #include <string.h>
 #include <math.h>

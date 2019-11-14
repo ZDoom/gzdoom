9eb878b439cec2e1b3358ea3324cfd436b8244b8
Major Cooke <majorcooke22@gmail.com>
2019-11-14 11:51:36 -0600

    - Fixed: Invulnerability checking for rails was done before the various THRU actor flags instead of after, meaning actors could block shots regardless of those flags. This was never intended.

--- src/playsim/p_map.cpp
+++ src/playsim/p_map.cpp
@@ -5089,12 +5089,6 @@ static ETraceStatus ProcessRailHit(FTraceResults &res, void *userdata)
 		return TRACE_Stop;
 	}
 
-	// Invulnerable things completely block the shot
-	if (data->StopAtInvul && res.Actor->flags2 & MF2_INVULNERABLE)
-	{
-		return TRACE_Stop;
-	}
-
 	// Skip actors if the puff has:
 	// 1. THRUACTORS (This one did NOT include a check for spectral)
 	// 2. MTHRUSPECIES on puff and the shooter has same species as the hit actor
@@ -5107,6 +5101,12 @@ static ETraceStatus ProcessRailHit(FTraceResults &res, void *userdata)
 		(data->ThruGhosts && res.Actor->flags3 & MF3_GHOST))
 	{
 		return TRACE_Skip;
+	}
+
+	// Invulnerable things completely block the shot
+	if (data->StopAtInvul && res.Actor->flags2 & MF2_INVULNERABLE)
+	{
+		return TRACE_Stop;
 	}
 
 	// Save this thing for damaging later, and continue the trace

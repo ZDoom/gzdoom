fdd17403e586e35c9c00f5b31e31df2e005378f0
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-11-26 12:05:29 +0200

    - added ability to force internal alternative HUD
    Set hud_althud_forceinternal CVAR to disable unwanted HUD customizations

--- src/g_statusbar/sbar.h
+++ src/g_statusbar/sbar.h
@@ -434,6 +434,8 @@ public:
 	virtual void SetMugShotState (const char *state_name, bool wait_till_done=false, bool reset=false);
 	void DrawLog();
 	uint32_t GetTranslation() const;
+
+	void CreateAltHUD();
 	void DrawAltHUD();
 
 	void DrawGraphic(FTextureID texture, double x, double y, int flags, double Alpha, double boxwidth, double boxheight, double scaleX, double scaleY);

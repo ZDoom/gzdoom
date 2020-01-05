3c4585932aa071e95690fbc2b72b2c3d097c33e1
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2020-01-05 16:48:17 -0500

    - bump for 4.3.1

--- src/version.h
+++ src/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.3.0"
+#define VERSIONSTR "4.3.1"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,3,0,0
-#define RC_PRODUCTVERSION 4,3,0,0
+#define RC_FILEVERSION 4,3,1,0
+#define RC_PRODUCTVERSION 4,3,1,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4

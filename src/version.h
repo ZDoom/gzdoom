97625090848a5abcd81a200bc355e65e0a2dfa7d
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-01-12 23:24:19 +0100

    4.3.2

--- src/version.h
+++ src/version.h
@@ -41,21 +41,21 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.3pre"
+#define VERSIONSTR "4.3.2"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,2,9999,0
-#define RC_PRODUCTVERSION 4,2,9999,0
+#define RC_FILEVERSION 4,3,2,0
+#define RC_PRODUCTVERSION 4,3,2,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
 #define VER_MINOR 3
-#define VER_REVISION 0
+#define VER_REVISION 2
 
 // This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!
 #define ENG_MAJOR 4
 #define ENG_MINOR 3
-#define ENG_REVISION 0
+#define ENG_REVISION 2
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you

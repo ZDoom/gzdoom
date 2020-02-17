9cb840eeb486cd997afe0288605a831a874e246e
johannes hanika <hanatos@gmail.com>
2020-02-17 16:25:18 +0100

    vid: remove explicit bit depth gl calls
    this makes gzdoom start on xorg configured with 30bit/pixel deep
    colour modes, too. it seems these explicit calls are not required,
    and detection via SDL_GetWindowPixelFormat does not yet work with
    extended bit depths, so detection + setting size to 8 or 10 depending
    on the result is unreliable too (as of SDL 2.0.1).

--- src/posix/sdl/sdlglvideo.cpp
+++ src/posix/sdl/sdlglvideo.cpp
@@ -190,10 +190,6 @@ namespace Priv
 
 	void SetupPixelFormat(int multisample, const int *glver)
 	{
-		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);

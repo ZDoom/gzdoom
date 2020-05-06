bc3947b64399dc5f239abd80c5efa127c26f98ee
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2020-05-06 05:50:00 -0400

    - inter_classic_scaling now defaults to true - fixed: last commit I accidentally left hardcoded testing values and did not change them back to check for the texture's original size

--- src/common/2d/v_2ddrawer.cpp
+++ src/common/2d/v_2ddrawer.cpp
@@ -757,8 +757,8 @@ void F2DDrawer::AddFlatFill(int left, int top, int right, int bottom, FGameTextu
 		float ar = 4.f / 3.f / (float)ActiveRatio((float)screen->GetWidth(), (float)screen->GetHeight());
 		fU1 = 0.f;
 		fV1 = 0.f;
-		fU2 = 320.f / 64.f * fs / ar;
-		fV2 = 200.f / 64.f * fs;
+		fU2 = 320.f / (float)src->GetDisplayWidth() * fs / ar;
+		fV2 = 200.f / (float)src->GetDisplayHeight() * fs;
 		break;
 	}
 	dg.mVertIndex = (int)mVertices.Reserve(4);

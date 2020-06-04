b441b649c4c99a4d016c208f54f501cd95707828
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-06-04 08:05:38 +0200

    - fixed some double to float conversion warnings.

--- src/common/utility/basics.h
+++ src/common/utility/basics.h
@@ -88,6 +88,10 @@ inline float RAD2DEG(float deg)
 	return deg * float(180. / M_PI);
 }
 
+inline double RAD2DEG(double deg)
+{
+	return deg * (180. / M_PI);
+}
 
 // Auto-registration sections for GCC.
 // Apparently, you cannot do string concatenation inside section attributes.

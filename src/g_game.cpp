82af7f4f68c8eb97af0b38f2c40126d3e28c1e74
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-06-10 12:54:17 +0200

    - mapped "Smooth mouse" back to m_filter CVAR and removed smooth_mouse.
    The option effectively disables per frame mouse checks.
    The motivation to add it was that many wireless mice 10-15 years ago had update rates of less than 35Hz, and on these it was necessary to sync mouse input with the playsim to properly interpolate between updates.
    Today this is totally useless and even counterproductive because modern mice have significantly higher update rates, so this option no longer smoothes things but instead makes the mouse feel more choppy.

--- src/g_game.cpp
+++ src/g_game.cpp
@@ -797,9 +797,6 @@ void G_BuildTiccmd (ticcmd_t *cmd)
 	cmd->ucmd.sidemove <<= 8;
 }
 
-//[Graf Zahl] This really helps if the mouse update rate can't be increased!
-CVAR (Bool,		smooth_mouse,	false,	CVAR_GLOBALCONFIG|CVAR_ARCHIVE)
-
 static int LookAdjust(int look)
 {
 	look <<= 16;
@@ -857,7 +854,7 @@ void G_AddViewPitch (int look, bool mouse)
 	}
 	if (look != 0)
 	{
-		LocalKeyboardTurner = (!mouse || smooth_mouse);
+		LocalKeyboardTurner = !mouse;
 	}
 }
 
@@ -872,7 +869,7 @@ void G_AddViewAngle (int yaw, bool mouse)
 	LocalViewAngle -= yaw;
 	if (yaw != 0)
 	{
-		LocalKeyboardTurner = (!mouse || smooth_mouse);
+		LocalKeyboardTurner = !mouse;
 	}
 }
 

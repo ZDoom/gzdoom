ec43ef38cf139e92e62c450210f715da962a7b32
vidaritos <31620802+vidaritos@users.noreply.github.com>
2020-09-18 17:53:45 +0200

    pass ScriptPosition down to color parser functions

--- src/scripting/thingdef_properties.cpp
+++ src/scripting/thingdef_properties.cpp
@@ -766,7 +766,7 @@ DEFINE_PROPERTY(translation, L, Actor)
 //==========================================================================
 DEFINE_PROPERTY(stencilcolor, C, Actor)
 {
-	PROP_COLOR_PARM(color, 0);
+	PROP_COLOR_PARM(color, 0, &bag.ScriptPosition);
 
 	defaults->fillcolor = color | (ColorMatcher.Pick (RPART(color), GPART(color), BPART(color)) << 24);
 }
@@ -776,7 +776,7 @@ DEFINE_PROPERTY(stencilcolor, C, Actor)
 //==========================================================================
 DEFINE_PROPERTY(bloodcolor, C, Actor)
 {
-	PROP_COLOR_PARM(color, 0);
+	PROP_COLOR_PARM(color, 0, &bag.ScriptPosition);
 
 	defaults->BloodColor = color;
 	defaults->BloodColor.a = 255;	// a should not be 0.
@@ -1608,7 +1608,7 @@ DEFINE_CLASS_PROPERTY_PREFIX(player, crouchsprite, S, PlayerPawn)
 //==========================================================================
 DEFINE_CLASS_PROPERTY_PREFIX(player, damagescreencolor, Cfs, PlayerPawn)
 {
-	PROP_COLOR_PARM(c, 0);
+	PROP_COLOR_PARM(c, 0, &bag.ScriptPosition);
 
 	PalEntry color = c;
 

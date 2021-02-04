6bc82bdeff7d6cf653b2d20c607ef3cb12c12952
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-02-04 12:00:06 +0200

    - fixed incorrect extension of overridden function prototype
    Prototype of overridden function with optional argument(s) missing could extend unrelated prototype of previously defined function when their arguments and return value(s) match
    
    https://forum.zdoom.org/viewtopic.php?t=71340

--- src/common/objects/dobjtype.cpp
+++ src/common/objects/dobjtype.cpp
@@ -706,13 +706,17 @@ int PClass::FindVirtualIndex(FName name, PFunction::Variant *variant, PFunction 
 						if (!(parentfunc->Variants[0].ArgFlags[a] & VARF_Optional)) return -1;
 					}
 
-					// Todo: extend the prototype
+					// Extend the prototype
+					TArray<PType*> argumentTypes = proto->ArgumentTypes;
+
 					for (unsigned a = proto->ArgumentTypes.Size(); a < vproto->ArgumentTypes.Size(); a++)
 					{
-						proto->ArgumentTypes.Push(vproto->ArgumentTypes[a]);
+						argumentTypes.Push(vproto->ArgumentTypes[a]);
 						variant->ArgFlags.Push(parentfunc->Variants[0].ArgFlags[a]);
 						variant->ArgNames.Push(NAME_None);
 					}
+
+					variant->Proto = NewPrototype(proto->ReturnTypes, argumentTypes);
 				}
 				return i;
 			}

869532538325c27b4226644ff0991c4feee466ad
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-04-26 09:46:20 +0300

    - fixed saving game to wrong save slot
    This is a temporary solution as it makes GZDoom and Raze code bases incompatible
    
    https://forum.zdoom.org/viewtopic.php?t=72085

--- src/common/menu/savegamemanager.cpp
+++ src/common/menu/savegamemanager.cpp
@@ -232,7 +232,6 @@ DEFINE_ACTION_FUNCTION(FSavegameManager, LoadSavegame)
 
 void FSavegameManagerBase::DoSave(int Selected, const char *savegamestring)
 {
-	RemoveNewSaveNode();
 	if (Selected != 0)
 	{
 		auto node = SaveGames[Selected];

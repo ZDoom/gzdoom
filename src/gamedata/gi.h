35d05d1b4f67c090c9625f5c4a25d99ee7e7592a
nashmuhandes <nashbackslash@gmail.com>
2021-05-28 19:01:49 +0800

    Add 'AddDialogues' in MAPINFO to additively add Strife NPC dialogs without overwriting each other.

--- src/gamedata/gi.h
+++ src/gamedata/gi.h
@@ -192,6 +192,7 @@ struct gameinfo_t
 	FString mMapArrow, mCheatMapArrow;
 	FString mEasyKey, mCheatKey;
 	FString Dialogue;
+	TArray<FString> AddDialogues;
 	FGIFont mStatscreenMapNameFont;
 	FGIFont mStatscreenFinishedFont;
 	FGIFont mStatscreenEnteringFont;

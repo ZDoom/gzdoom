30e24938ca4e2a572de17f9ce7c5dac9e138c617
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-05-31 22:13:03 +0200

    - let dynamic lights call UpdateLocation instead of just setting their position right after being spawned.
    This ensures that the position is correct and that everything gets set up properly.

--- src/playsim/a_dynlight.cpp
+++ src/playsim/a_dynlight.cpp
@@ -748,7 +748,7 @@ void AActor::AttachLight(unsigned int count, const FLightDefaults *lightdef)
 		AttachedLights.Push(light);
 	}
 	lightdef->ApplyProperties(light);
-	light->Pos = Pos();
+	light->UpdateLocation();
 }
 
 //==========================================================================

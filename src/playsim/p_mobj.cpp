23477342c3e254cc3c8274f157eae80d40bfef8c
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-09-24 00:01:19 +0200

    - fixed handling of states with the 'light' keyword.

--- src/playsim/p_mobj.cpp
+++ src/playsim/p_mobj.cpp
@@ -584,7 +584,7 @@ bool AActor::SetState (FState *newstate, bool nofunction)
 		newstate = newstate->GetNextState();
 	} while (tics == 0);
 
-	if (GetInfo()->LightAssociations.Size())
+	if (GetInfo()->LightAssociations.Size() || newstate->Light > 0)
 	{
 		flags8 |= MF8_RECREATELIGHTS;
 		Level->flags3 |= LEVEL3_LIGHTCREATED;

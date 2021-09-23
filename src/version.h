708717b6ab5278f07738fc9e5c15c95e6412a4d4
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-09-23 14:01:14 +0200

    - we're past 4.7 now.

--- src/version.h
+++ src/version.h
@@ -41,20 +41,20 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.7pre"
+#define VERSIONSTR "4.8pre"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,6,9999,0
-#define RC_PRODUCTVERSION 4,6,9999,0
+#define RC_FILEVERSION 4,7,9999,0
+#define RC_PRODUCTVERSION 4,7,9999,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
-#define VER_MINOR 7
+#define VER_MINOR 8
 #define VER_REVISION 0
 
 // This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!
 #define ENG_MAJOR 4
-#define ENG_MINOR 7
+#define ENG_MINOR 8
 #define ENG_REVISION 0
 
 // Version identifier for network games.

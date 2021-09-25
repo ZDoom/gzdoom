b59554f319d747f55022113917baaefe11f2080c
Magnus Norddahl <dpjudas@users.noreply.github.com>
2021-09-25 14:28:44 +0200

    Fix "out of memory" errors when the vulkan backend is asked to create zero byte sized buffers

--- src/common/rendering/vulkan/system/vk_builders.h
+++ src/common/rendering/vulkan/system/vk_builders.h
@@ -571,7 +571,7 @@ inline BufferBuilder::BufferBuilder()
 
 inline void BufferBuilder::setSize(size_t size)
 {
-	bufferInfo.size = size;
+	bufferInfo.size = std::max(size, (size_t)16);
 }
 
 inline void BufferBuilder::setUsage(VkBufferUsageFlags bufferUsage, VmaMemoryUsage memoryUsage, VmaAllocationCreateFlags allocFlags)

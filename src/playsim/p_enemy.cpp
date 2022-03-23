196d2efc86f0d2e979cda22470852b29e0be30d6
RaveYard <MrRaveYard@gmail.com>
2022-03-23 19:56:27 +0100

    Add CHF_DONTIDLE for A_Chase

--- src/playsim/p_enemy.cpp
+++ src/playsim/p_enemy.cpp
@@ -2189,6 +2189,7 @@ enum ChaseFlags
 	CHF_NODIRECTIONTURN = 64,
 	CHF_NOPOSTATTACKTURN = 128,
 	CHF_STOPIFBLOCKED = 256,
+	CHF_DONTIDLE = 512,
 };
 
 void A_Wander(AActor *self, int flags)
@@ -2430,7 +2431,7 @@ void A_DoChase (AActor *actor, bool fastchase, FState *meleestate, FState *missi
 		}
 		if (actor->target == NULL)
 		{
-			if (actor->flags & MF_FRIENDLY)
+			if (flags & CHF_DONTIDLE || actor->flags & MF_FRIENDLY)
 			{
 				//A_Look(actor);
 				if (actor->target == NULL)

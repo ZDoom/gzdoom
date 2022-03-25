eeb67bcec38257469af60c97f9ce8725a2beb5e0
Marisa Kirisame <marisa@sayachan.org>
2022-03-25 17:01:39 +0100

    Use RDTSC equivalent on AARCH64.

--- src/common/platform/posix/sdl/i_system.cpp
+++ src/common/platform/posix/sdl/i_system.cpp
@@ -144,7 +144,15 @@ void CalculateCPUSpeed()
 {
 	PerfAvailable = false;
 	PerfToMillisec = PerfToSec = 0.;
-#ifdef __linux__
+#ifdef __aarch64__
+	// [MK] on aarch64 rather than having to calculate cpu speed, there is
+	// already an independent frequency for the perf timer
+	uint64_t frq;
+	asm volatile("mrs %0, cntfrq_el0":"=r"(frq));
+	PerfAvailable = true;
+	PerfToSec = 1./frq;
+	PerfToMillisec = PerfToSec*1000.;
+#elif defined(__linux__)
 	// [MK] read from perf values if we can
 	struct perf_event_attr pe;
 	memset(&pe,0,sizeof(struct perf_event_attr));

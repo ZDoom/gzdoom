77a4bc16cfe2f185b85c63b3d0363a97a6bd1a99
Christoph Oelckers <coelckers@users.noreply.github.com>
2022-05-23 00:08:33 +0200

    - fixed handling of automap sprites with negative scale.

--- src/am_map.cpp
+++ src/am_map.cpp
@@ -3025,6 +3025,11 @@ void DAutomap::DrawMarker (FGameTexture *tex, double x, double y, int yadjust,
 	{
 		return;
 	}
+	if (xscale < 0)
+	{
+		flip = !flip;
+		xscale = -xscale;
+	}
 	if (am_rotate == 1 || (am_rotate == 2 && viewactive))
 	{
 		rotatePoint (&x, &y);

2c69afa1187f55c2634c67c3ece47d89fd1b4f67
Christoph Oelckers <coelckers@users.noreply.github.com>
2022-05-31 11:00:10 +0200

    - fixed SDL version check

--- src/common/platform/posix/sdl/sdlglvideo.cpp
+++ src/common/platform/posix/sdl/sdlglvideo.cpp
@@ -394,9 +394,7 @@ SDLVideo::SDLVideo ()
 	}
 
 	// Fail gracefully if we somehow reach here after linking against a SDL2 library older than 2.0.6.
-	SDL_version sdlver;
-	SDL_GetVersion(&sdlver);
-	if (!(sdlver.patch >= 6))
+	if (!SDL_VERSION_ATLEAST(2, 0, 6))
 	{
 		I_FatalError("Only SDL 2.0.6 or later is supported.");
 	}

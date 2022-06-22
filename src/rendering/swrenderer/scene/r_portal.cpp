67127e4cced0f7c90cafda7837d46771e27b382f
Sally Coolatta <tehrealsalt@gmail.com>
2022-06-22 15:59:07 -0400

    Bugfix for retro software mode mirrors not working

--- src/rendering/swrenderer/scene/r_portal.cpp
+++ src/rendering/swrenderer/scene/r_portal.cpp
@@ -329,9 +329,9 @@ namespace swrenderer
 		DAngle startang = viewpoint.Angles.Yaw;
 		DVector3 startpos = viewpoint.Pos;
 		DVector3 savedpath[2] = { viewpoint.Path[0], viewpoint.Path[1] };
-		ActorRenderFlags savedvisibility = viewpoint.camera ? viewpoint.camera->renderflags & RF_INVISIBLE : ActorRenderFlags::FromInt(0);
+		ActorRenderFlags savedvisibility = viewpoint.camera ? viewpoint.camera->renderflags & RF_MAYBEINVISIBLE : ActorRenderFlags::FromInt(0);
 		
-		viewpoint.camera->renderflags &= ~RF_INVISIBLE;
+		viewpoint.camera->renderflags &= ~RF_MAYBEINVISIBLE;
 
 		CurrentPortalUniq++;
 
@@ -437,7 +437,7 @@ namespace swrenderer
 
 		Thread->OpaquePass->RenderScene(Thread->Viewport->Level());
 		Thread->Clip3D->ResetClip(); // reset clips (floor/ceiling)
-		if (!savedvisibility && viewpoint.camera) viewpoint.camera->renderflags &= ~RF_INVISIBLE;
+		if (!savedvisibility && viewpoint.camera) viewpoint.camera->renderflags &= ~RF_MAYBEINVISIBLE;
 
 		Thread->PlaneList->Render();
 		RenderPlanePortals();

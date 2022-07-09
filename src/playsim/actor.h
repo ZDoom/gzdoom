9ab65578226b69583ca257edbe96f260ac365e57
Shiny Metagross <30511800+ShinyMetagross@users.noreply.github.com>
2022-07-08 22:47:12 -0700

    A_ChangeModel now has generatorIndex
    - To really take advantage of this function, I thought it would be useful to be able to add additional models if the user wants to. Let's say you got a player model at index 0. Your gun model has the same frames, but you don't want to duplicate the modeldef data. With generator index, you don't need to duplicate the data, just tell generator index to clone frame data from index 0.
    - Implemented a little something to check if a negative skin or model index were passed, and prevent modders from pulling that off.

--- src/playsim/actor.h
+++ src/playsim/actor.h
@@ -682,6 +682,7 @@ public:
 	TArray<int>			modelIDs;
 	TArray<FTextureID>	skinIDs;
 	TArray<FTextureID>	surfaceSkinIDs;
+	TArray<int>			modelFrameGenerators;
 
 	DActorModelData() = default;
 	virtual void Serialize(FSerializer& arc) override;

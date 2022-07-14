fbd604b72569e56c67dd8512e6eec10ce07644b0
Christoph Oelckers <coelckers@users.noreply.github.com>
2022-07-14 08:13:54 +0200

    - fixed handling of *dive and *surface sounds.
    The old water level needs to be retrieved before handling splashes, because SplashCheck already updates the waterlevel.
    This was swapped around by a recent PR.

--- src/playsim/p_mobj.cpp
+++ src/playsim/p_mobj.cpp
@@ -4388,9 +4388,9 @@ void AActor::SplashCheck()
 
 bool AActor::UpdateWaterLevel(bool dosplash)
 {
-	if (dosplash) SplashCheck();
-
 	int oldlevel = waterlevel;
+
+	if (dosplash) SplashCheck();
 	UpdateWaterDepth(false);
 
 	// Play surfacing and diving sounds, as appropriate.

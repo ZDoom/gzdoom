524a247b059b85f3de9583890841b7d0a2b9b38b
Marisa Heit <rheit@users.noreply.github.com>
2022-07-28 23:17:19 -0500

    Fix definition order of ZScript structs
    Do a first pass over the Structs array in CompileAllFields() to reorder them such that if a struct uses other structs, those structs will be resolved first.

--- src/common/scripting/frontend/zcc_compile.h
+++ src/common/scripting/frontend/zcc_compile.h
@@ -135,6 +135,9 @@ protected:
 	PType *DetermineType(PType *outertype, ZCC_TreeNode *field, FName name, ZCC_Type *ztype, bool allowarraytypes, bool formember);
 	PType *ResolveArraySize(PType *baseType, ZCC_Expression *arraysize, PContainerType *cls, bool *nosize);
 	PType *ResolveUserType(ZCC_BasicType *type, PSymbolTable *sym, bool nativetype);
+	TArray<ZCC_StructWork *> OrderStructs();
+	void AddStruct(TArray<ZCC_StructWork *> &new_order, ZCC_StructWork *struct_def);
+	ZCC_StructWork *StructTypeToWork(const PStruct *type) const;
 
 	void CompileFunction(ZCC_StructWork *c, ZCC_FuncDeclarator *f, bool forclass);
 

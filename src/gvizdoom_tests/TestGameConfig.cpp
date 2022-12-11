5914b543e9db432af9e1187f1c15317e095c9fd5
Lehdari <miika.lehtimaki@gmail.com>
2022-12-12 01:28:54 +0200

    TestGameConfig: Add test for video parameters

--- src/gvizdoom_tests/TestGameConfig.cpp
+++ src/gvizdoom_tests/TestGameConfig.cpp
@@ -31,3 +31,77 @@ TEST(TestGameConfig, DefaultValues)
 
     ASSERT_EQ(hash, 0x8ef0b3cb);
 }
+
+// Test different video parameters
+TEST(TestGameConfig, VideoParams)
+{
+    auto& doomGame = DoomGame::instance();
+    GameConfig gameConfig;
+
+    {
+        // 320x240, truecolor
+        gameConfig.videoWidth = 320;
+        gameConfig.videoHeight = 240;
+        gameConfig.videoTrueColor = true;
+        doomGame.init(gameConfig);
+        doomGame.update(Action()); // pass a single default action
+
+        auto hash = xorHash(
+            reinterpret_cast<const uint32_t*>(doomGame.getPixelsBGRA()),
+            doomGame.getScreenWidth() * doomGame.getScreenHeight());
+
+        ASSERT_EQ(doomGame.getScreenWidth(), gameConfig.videoWidth);
+        ASSERT_EQ(doomGame.getScreenHeight(), gameConfig.videoHeight);
+        ASSERT_EQ(hash, 0xd16d13cf);
+    }
+    {
+        // 1280x960, truecolor
+        gameConfig.videoWidth = 1280;
+        gameConfig.videoHeight = 960;
+        gameConfig.videoTrueColor = true;
+        doomGame.restart(gameConfig);
+        doomGame.update(Action()); // pass a single default action
+
+        auto hash = xorHash(
+            reinterpret_cast<const uint32_t*>(doomGame.getPixelsBGRA()),
+            doomGame.getScreenWidth() * doomGame.getScreenHeight());
+
+        ASSERT_EQ(doomGame.getScreenWidth(), gameConfig.videoWidth);
+        ASSERT_EQ(doomGame.getScreenHeight(), gameConfig.videoHeight);
+        ASSERT_EQ(hash, 0x08aefaae);
+    }
+#if 0 // TODO for some reason the paletted rendering produces indeterministic results, subject to investigation
+    {
+        // 320x240, 256-color palette
+        gameConfig.videoWidth = 320;
+        gameConfig.videoHeight = 240;
+        gameConfig.videoTrueColor = false;
+        doomGame.restart(gameConfig);
+        doomGame.update(Action()); // pass a single default action
+
+        auto hash = xorHash(
+            reinterpret_cast<const uint32_t*>(doomGame.getPixelsBGRA()),
+            doomGame.getScreenWidth() * doomGame.getScreenHeight());
+
+        ASSERT_EQ(doomGame.getScreenWidth(), gameConfig.videoWidth);
+        ASSERT_EQ(doomGame.getScreenHeight(), gameConfig.videoHeight);
+        ASSERT_EQ(hash, 0x9450d1e1);
+    }
+    {
+        // 1280x960, 256-color palette
+        gameConfig.videoWidth = 1280;
+        gameConfig.videoHeight = 960;
+        gameConfig.videoTrueColor = false;
+        doomGame.restart(gameConfig);
+        doomGame.update(Action()); // pass a single default action
+
+        auto hash = xorHash(
+            reinterpret_cast<const uint32_t*>(doomGame.getPixelsBGRA()),
+            doomGame.getScreenWidth() * doomGame.getScreenHeight());
+
+        ASSERT_EQ(doomGame.getScreenWidth(), gameConfig.videoWidth);
+        ASSERT_EQ(doomGame.getScreenHeight(), gameConfig.videoHeight);
+        ASSERT_EQ(hash, 0x258e622a);
+    }
+#endif
+}

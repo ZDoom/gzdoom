aa061562cd647e7dee6caf2faf2aae8f4a8e3c32
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2023-01-14 11:23:02 -0500

    - manually reimplement pull request #1850

--- src/common/rendering/vulkan/shaders/vk_shader.cpp
+++ src/common/rendering/vulkan/shaders/vk_shader.cpp
@@ -244,6 +244,7 @@ static const char *shaderBindings = R"(
 	layout(set = 2, binding = 8) uniform sampler2D texture9;
 	layout(set = 2, binding = 9) uniform sampler2D texture10;
 	layout(set = 2, binding = 10) uniform sampler2D texture11;
+	layout(set = 2, binding = 11) uniform sampler2D texture12;
 
 	// This must match the PushConstants struct
 	layout(push_constant) uniform PushConstants

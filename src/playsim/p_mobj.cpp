52460b90a233485f738fa4fa8a4f86de27bd0dd7
Boondorl <59555366+Boondorl@users.noreply.github.com>
2023-01-19 11:44:57 -0500

    Fixed BlockingLine giving erroneous results
    Fixed BlockingLine not clearing properly when xy velocity is 0

--- src/playsim/p_mobj.cpp
+++ src/playsim/p_mobj.cpp
@@ -1950,6 +1950,7 @@ static double P_XYMovement (AActor *mo, DVector2 scroll)
 	FCheckPosition tm(!!(mo->flags2 & MF2_RIP));
 
 	DAngle oldangle = mo->Angles.Yaw;
+	line_t* blocker = nullptr;
 	do
 	{
 		if (mo->Level->i_compatflags & COMPATF_WALLRUN) pushtime++;
@@ -1965,7 +1966,7 @@ static double P_XYMovement (AActor *mo, DVector2 scroll)
 		{
 			// blocked move
 			AActor *BlockingMobj = mo->BlockingMobj;
-			line_t *BlockingLine = mo->BlockingLine;
+			line_t *BlockingLine = blocker = mo->BlockingLine;
 
 			// [ZZ] 
 			if (!BlockingLine && !BlockingMobj) // hit floor or ceiling while XY movement - sector actions
@@ -2212,6 +2213,9 @@ explode:
 			}
 		}
 	} while (++step <= steps);
+
+	// Correctly update whether the Actor was truly blocked by a wall or not
+	mo->BlockingLine = blocker;
 
 	// Friction
 
@@ -4084,6 +4088,7 @@ void AActor::Tick ()
 
 		// Handle X and Y velocities
 		BlockingMobj = nullptr;
+		BlockingLine = nullptr;
 		sector_t* oldBlockingCeiling = BlockingCeiling;
 		sector_t* oldBlockingFloor = BlockingFloor;
 		Blocking3DFloor = nullptr;

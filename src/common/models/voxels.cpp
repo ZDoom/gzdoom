a44f7a72cabd503e530957de3e24610f674da4f9
Christoph Oelckers <coelckers@users.noreply.github.com>
2023-09-10 21:31:53 +0200

    - fixed voxel loader

--- src/common/models/voxels.cpp
+++ src/common/models/voxels.cpp
@@ -163,7 +163,7 @@ FVoxel *R_LoadKVX(int lumpnum)
 
 	auto lump =  fileSystem.ReadFile(lumpnum);	// FileData adds an extra 0 byte to the end.
 	auto rawvoxel = lump.GetBytes();
-	int voxelsize = (int)(lump.GetSize()-1);
+	int voxelsize = (int)(lump.GetSize());
 
 	// Oh, KVX, why couldn't you have a proper header? We'll just go through
 	// and collect each MIP level, doing lots of range checking, and if the

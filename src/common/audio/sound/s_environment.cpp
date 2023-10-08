624d4cd713eae69291871c18691359c74432a2f7
Christoph Oelckers <coelckers@users.noreply.github.com>
2023-10-08 11:54:14 +0200

    use copystring instead of strdup when reading environments.
    The mismatched the underlying code which allocates via copystring.

--- src/common/audio/sound/s_environment.cpp
+++ src/common/audio/sound/s_environment.cpp
@@ -538,7 +538,7 @@ void S_ReadReverbDef (FScanner &sc)
 
 	while (sc.GetString ())
 	{
-		name = strdup (sc.String);
+		name = copystring(sc.String);
 		sc.MustGetNumber ();
 		id1 = sc.Number;
 		sc.MustGetNumber ();

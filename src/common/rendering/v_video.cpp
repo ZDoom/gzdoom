747e55ce6001b28758611ed31a1b92725fe4428b
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2023-11-13 17:11:47 -0500

    - fix GL ES switch appearing as 'Unknown' in the menu sometimes

--- src/common/rendering/v_video.cpp
+++ src/common/rendering/v_video.cpp
@@ -104,6 +104,7 @@ CUSTOM_CVAR(Int, vid_preferbackend, 1, CVAR_ARCHIVE | CVAR_GLOBALCONFIG | CVAR_N
 #ifdef HAVE_GLES2
 	case 3:
 	case 2:
+		self = 2;
 		Printf("Selecting OpenGLES 2.0 backend...\n");
 		break;
 #endif
@@ -122,7 +123,7 @@ CUSTOM_CVAR(Int, vid_preferbackend, 1, CVAR_ARCHIVE | CVAR_GLOBALCONFIG | CVAR_N
 int V_GetBackend()
 {
 	int v = vid_preferbackend;
-	if (v == 3) v = 2;
+	if (v == 3) vid_preferbackend = v = 2;
 	else if (v < 0 || v > 3) v = 0;
 	return v;
 }

44c22a5ef60e14e494bfe5c3f9ce731ebd93f49f
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2024-01-09 03:53:13 -0300

    rename SPF_STANDALONE_ANIMATIONS to SPF_LOCAL_ANIM

--- src/playsim/p_effect.h
+++ src/playsim/p_effect.h
@@ -64,7 +64,7 @@ enum EParticleFlags
 	SPF_ROLL					= 1 << 6,
 	SPF_REPLACE					= 1 << 7,
 	SPF_NO_XY_BILLBOARD			= 1 << 8,
-	SPF_STANDALONE_ANIMATIONS	= 1 << 9,
+	SPF_LOCAL_ANIM				= 1 << 9,
 };
 
 class DVisualThinker;

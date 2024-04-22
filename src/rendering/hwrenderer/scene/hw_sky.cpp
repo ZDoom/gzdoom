d45a4f18d46f482847f8d54300c87d9dafd7cadc
Christoph Oelckers <coelckers@users.noreply.github.com>
2024-04-22 07:14:04 +0200

    fix lifetime of skyinfo variable in HWWall::SkyPlane.

--- src/rendering/hwrenderer/scene/hw_sky.cpp
+++ src/rendering/hwrenderer/scene/hw_sky.cpp
@@ -137,9 +137,9 @@ void HWWall::SkyPlane(HWWallDispatcher *di, sector_t *sector, int plane, bool al
 	// Either a regular sky or a skybox with skyboxes disabled
 	if ((sportal == nullptr && sector->GetTexture(plane) == skyflatnum) || (gl_noskyboxes && sportal != nullptr && sportal->mType == PORTS_SKYVIEWPOINT))
 	{
+		HWSkyInfo skyinfo;
 		if (di->di)
 		{
-			HWSkyInfo skyinfo;
 			skyinfo.init(di->di, sector, plane, sector->skytransfer, Colormap.FadeColor);
 			ptype = PORTALTYPE_SKY;
 			sky = &skyinfo;

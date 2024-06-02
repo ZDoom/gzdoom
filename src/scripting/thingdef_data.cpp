f13bc09840c1c215587fba2fd40e22f16a9c076b
jekyllgrim <jekyllgrim@gmail.com>
2024-06-03 01:47:41 +0300

    Add bIsPuff flag

--- src/scripting/thingdef_data.cpp
+++ src/scripting/thingdef_data.cpp
@@ -354,6 +354,7 @@ static FFlagDef ActorFlagDefs[]=
 	DEFINE_FLAG(MF9, SHADOWAIMVERT, AActor, flags9),
 	DEFINE_FLAG(MF9, DECOUPLEDANIMATIONS, AActor, flags9),
 	DEFINE_FLAG(MF9, NOSECTORDAMAGE, AActor, flags9),
+	DEFINE_FLAG(MF9, ISPUFF, AActor, flags9), //[AA] was spawned by SpawnPuff
 
 	// Effect flags
 	DEFINE_FLAG(FX, VISIBILITYPULSE, AActor, effects),

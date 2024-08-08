8043370ef1868f55bf03b2123b95f691dc619f16
nashmuhandes <nashbackslash@gmail.com>
2024-08-09 03:44:46 +0800

    Export PlayDiveOrSurfaceSounds to ZScript and made it virtual for mods to customize the diving and surfacing sounds

--- src/playsim/actor.h
+++ src/playsim/actor.h
@@ -1411,6 +1411,7 @@ public:
 	bool IsMapActor();
 	bool SetState (FState *newstate, bool nofunction=false);
 	void SplashCheck();
+	void PlayDiveOrSurfaceSounds(int oldlevel = 0);
 	bool UpdateWaterLevel (bool splash=true);
 	bool isFast();
 	bool isSlow();

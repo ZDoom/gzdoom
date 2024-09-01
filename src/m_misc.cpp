5fdba4ceab7cb3a132ecaa1b3171083b2cf3c33b
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2024-08-31 21:14:30 -0300

    remove screenshot_type and pcx saving

--- src/m_misc.cpp
+++ src/m_misc.cpp
@@ -70,7 +70,6 @@
 FGameConfigFile *GameConfig;
 
 CVAR(Bool, screenshot_quiet, false, CVAR_ARCHIVE|CVAR_GLOBALCONFIG);
-CVAR(String, screenshot_type, "png", CVAR_ARCHIVE|CVAR_GLOBALCONFIG);
 CVAR(String, screenshot_dir, "", CVAR_ARCHIVE|CVAR_GLOBALCONFIG);
 EXTERN_CVAR(Bool, longsavemessages);
 
@@ -333,190 +332,19 @@ void M_LoadDefaults ()
 // SCREEN SHOTS
 //
 
-
-struct pcx_t
-{
-	int8_t				manufacturer;
-	int8_t				version;
-	int8_t				encoding;
-	int8_t				bits_per_pixel;
-
-	uint16_t			xmin;
-	uint16_t			ymin;
-	uint16_t			xmax;
-	uint16_t			ymax;
-	
-	uint16_t			hdpi;
-	uint16_t			vdpi;
-
-	uint8_t				palette[48];
-	
-	int8_t				reserved;
-	int8_t				color_planes;
-	uint16_t			bytes_per_line;
-	uint16_t			palette_type;
-	
-	int8_t				filler[58];
-};
-
-
-inline void putc(unsigned char chr, FileWriter *file)
-{
-	file->Write(&chr, 1);
-}
-
-//
-// WritePCXfile
-//
-void WritePCXfile (FileWriter *file, const uint8_t *buffer, const PalEntry *palette,
-				   ESSType color_type, int width, int height, int pitch)
-{
-	TArray<uint8_t> temprow_storage(width * 3, true);
-	uint8_t *temprow = &temprow_storage[0];
-	const uint8_t *data;
-	int x, y;
-	int runlen;
-	int bytes_per_row_minus_one;
-	uint8_t color;
-	pcx_t pcx;
-
-	pcx.manufacturer = 10;				// PCX id
-	pcx.version = 5;					// 256 (or more) colors
-	pcx.encoding = 1;
-	pcx.bits_per_pixel = 8;				// 256 (or more) colors
-	pcx.xmin = 0;
-	pcx.ymin = 0;
-	pcx.xmax = LittleShort((unsigned short)(width-1));
-	pcx.ymax = LittleShort((unsigned short)(height-1));
-	pcx.hdpi = LittleShort((unsigned short)75);
-	pcx.vdpi = LittleShort((unsigned short)75);
-	memset (pcx.palette, 0, sizeof(pcx.palette));
-	pcx.reserved = 0;
-	pcx.color_planes = (color_type == SS_PAL) ? 1 : 3;	// chunky image
-	pcx.bytes_per_line = width + (width & 1);
-	pcx.palette_type = 1;				// not a grey scale
-	memset (pcx.filler, 0, sizeof(pcx.filler));
-
-	file->Write(&pcx, 128);
-
-	bytes_per_row_minus_one = ((color_type == SS_PAL) ? width : width * 3) - 1;
-
-	// pack the image
-	for (y = height; y > 0; y--)
-	{
-		switch (color_type)
-		{
-		case SS_PAL:
-			data = buffer;
-			break;
-
-		case SS_RGB:
-			// Unpack RGB into separate planes.
-			for (int i = 0; i < width; ++i)
-			{
-				temprow[i            ] = buffer[i*3];
-				temprow[i + width    ] = buffer[i*3 + 1];
-				temprow[i + width * 2] = buffer[i*3 + 2];
-			}
-			data = temprow;
-			break;
-
-		case SS_BGRA:
-			// Unpack RGB into separate planes, discarding A.
-			for (int i = 0; i < width; ++i)
-			{
-				temprow[i            ] = buffer[i*4 + 2];
-				temprow[i + width    ] = buffer[i*4 + 1];
-				temprow[i + width * 2] = buffer[i*4];
-			}
-			data = temprow;
-			break;
-
-		default:
-			// Should never happen.
-			return;
-		}
-		buffer += pitch;
-
-		color = *data++;
-		runlen = 1;
-
-		for (x = bytes_per_row_minus_one; x > 0; x--)
-		{
-			if (*data == color)
-			{
-				runlen++;
-			}
-			else
-			{
-				if (runlen > 1 || color >= 0xc0)
-				{
-					while (runlen > 63)
-					{
-						putc (0xff, file);
-						putc (color, file);
-						runlen -= 63;
-					}
-					if (runlen > 0)
-					{
-						putc (0xc0 + runlen, file);
-					}
-				}
-				if (runlen > 0)
-				{
-					putc (color, file);
-				}
-				runlen = 1;
-				color = *data;
-			}
-			data++;
-		}
-
-		if (runlen > 1 || color >= 0xc0)
-		{
-			while (runlen > 63)
-			{
-				putc (0xff, file);
-				putc (color, file);
-				runlen -= 63;
-			}
-			if (runlen > 0)
-			{
-				putc (0xc0 + runlen, file);
-			}
-		}
-		if (runlen > 0)
-		{
-			putc (color, file);
-		}
-
-		if (width & 1)
-			putc (0, file);
-	}
-
-	// write the palette
-	if (color_type == SS_PAL)
-	{
-		putc (12, file);		// palette ID byte
-		for (x = 0; x < 256; x++, palette++)
-		{
-			putc (palette->r, file);
-			putc (palette->g, file);
-			putc (palette->b, file);
-		}
-	}
-}
-
 //
 // WritePNGfile
 //
 void WritePNGfile (FileWriter *file, const uint8_t *buffer, const PalEntry *palette,
 				   ESSType color_type, int width, int height, int pitch, float gamma)
 {
-	char software[100];
-	mysnprintf(software, countof(software), GAMENAME " %s", GetVersionString());
+	FString software(GAMENAME " ");
+
+	software += GetVersionString();
+
+
 	if (!M_CreatePNG (file, buffer, palette, color_type, width, height, pitch, gamma) ||
-		!M_AppendPNGText (file, "Software", software) ||
+		!M_AppendPNGText (file, "Software", software.GetChars()) ||
 		!M_FinishPNG (file))
 	{
 		Printf ("%s\n", GStrings.GetString("TXT_SCREENSHOTERR"));
@@ -574,7 +402,6 @@ void M_ScreenShot (const char *filename)
 {
 	FileWriter *file;
 	FString autoname;
-	bool writepcx = (stricmp (screenshot_type, "pcx") == 0);	// PNG is the default
 
 	// find a file name to save it to
 	if (filename == NULL || filename[0] == '\0')
@@ -600,7 +427,7 @@ void M_ScreenShot (const char *filename)
 		}
 		autoname = NicePath(autoname.GetChars());
 		CreatePath(autoname.GetChars());
-		if (!FindFreeName (autoname, writepcx ? "pcx" : "png"))
+		if (!FindFreeName (autoname, "png"))
 		{
 			Printf ("M_ScreenShot: Delete some screenshots\n");
 			return;
@@ -609,7 +436,7 @@ void M_ScreenShot (const char *filename)
 	else
 	{
 		autoname = filename;
-		DefaultExtension (autoname, writepcx ? ".pcx" : ".png");
+		DefaultExtension (autoname, ".png");
 	}
 
 	// save the screenshot
@@ -626,16 +453,10 @@ void M_ScreenShot (const char *filename)
 			Printf ("Could not open %s\n", autoname.GetChars());
 			return;
 		}
-		if (writepcx)
-		{
-			WritePCXfile(file, buffer.Data(), nullptr, color_type,
-				screen->GetWidth(), screen->GetHeight(), pitch);
-		}
-		else
-		{
-			WritePNGfile(file, buffer.Data(), nullptr, color_type,
-				screen->GetWidth(), screen->GetHeight(), pitch, gamma);
-		}
+
+		WritePNGfile(file, buffer.Data(), nullptr, color_type,
+			screen->GetWidth(), screen->GetHeight(), pitch, gamma);
+		
 		delete file;
 
 		if (!screenshot_quiet)

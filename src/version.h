573cd2f12c7549ebe323366f0dcd8642614f9abb
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2024-10-12 01:30:20 -0400

    g4.14pre

--- src/version.h
+++ src/version.h
@@ -41,20 +41,20 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.13pre"
+#define VERSIONSTR "4.14pre"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,12,9999,0
-#define RC_PRODUCTVERSION 4,12,9999,0
+#define RC_FILEVERSION 4,13,9999,0
+#define RC_PRODUCTVERSION 4,13,9999,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
-#define VER_MINOR 13
+#define VER_MINOR 14
 #define VER_REVISION 0
 
 // This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!
 #define ENG_MAJOR 4
-#define ENG_MINOR 13
+#define ENG_MINOR 14
 #define ENG_REVISION 0
 
 // Version identifier for network games.

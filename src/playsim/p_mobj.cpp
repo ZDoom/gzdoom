dd740b59e0d6af236ab087d507c8750ac5a95cb9
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2024-10-19 10:17:01 -0400

    Revert "- simplify and deconstruct logic for applying sector damage - also fixes voodoo doll sector damage in TNT MAP30"
    This reverts commit 3e33e31d1961f366229d5b4848fa29d2d797b8c3.
    
    Revert "Added MF9_FORCESECTORDAMAGE."
    
    This reverts commit 61bd3a739a0ca7509db46ddabbff1fc35bfd8908.

--- src/playsim/p_mobj.cpp
+++ src/playsim/p_mobj.cpp
@@ -4436,11 +4436,7 @@ void AActor::Tick ()
 		if (ObjectFlags & OF_EuthanizeMe) return;
 	}
 	//[inkoalawetrust] Genericized level damage handling that makes sector, 3D floor, and TERRAIN flat damage affect monsters and other NPCs too.
-	bool afsdnope = !!(flags9 & MF9_NOSECTORDAMAGE);
-	bool afsdforce = !!(flags9 & MF9_FORCESECTORDAMAGE);
-	bool sfhurtmonsters = !!(Sector->MoreFlags & SECMF_HURTMONSTERS);
-	bool isplayer = (player != nullptr) && (this == player->mo);
-	if ((!afsdnope || afsdforce) && (isplayer || sfhurtmonsters || afsdforce))
+	if (!(flags9 & MF9_NOSECTORDAMAGE) && (player || (player == nullptr && Sector->MoreFlags & SECMF_HURTMONSTERS)))
 	{
 		P_ActorOnSpecial3DFloor(this);
 		P_ActorInSpecialSector(this,Sector);

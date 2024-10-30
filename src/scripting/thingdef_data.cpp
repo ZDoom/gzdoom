864a5ab6ae87f8a12e3c998df7cdb1816146eedb
Major Cooke <majorcooke22@gmail.com>
2024-10-30 16:22:33 -0500

    Fixed STRETCHPIXELS flag not being exposed.

--- src/scripting/thingdef_data.cpp
+++ src/scripting/thingdef_data.cpp
@@ -387,6 +387,7 @@ static FFlagDef ActorFlagDefs[]=
 	DEFINE_FLAG(RF2, CAMFOLLOWSPLAYER, AActor, renderflags2),
 	DEFINE_FLAG(RF2, ISOMETRICSPRITES, AActor, renderflags2),
 	DEFINE_FLAG(RF2, SQUAREPIXELS, AActor, renderflags2),
+	DEFINE_FLAG(RF2, STRETCHPIXELS, AActor, renderflags2),
 
 	// Bounce flags
 	DEFINE_FLAG2(BOUNCE_Walls, BOUNCEONWALLS, AActor, BounceFlags),

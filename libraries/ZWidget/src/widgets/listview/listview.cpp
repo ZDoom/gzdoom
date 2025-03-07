a8255bc4f11c70705928e9ef5020066df0fcb14e
Boondorl <boondorl@live.com>
2025-03-06 19:41:40 -0500

    Updated ZWidget ListView
    Added column support for consistent spacing between elements. Improved item adding functionality. Added Update and Remove item functionalities. Update ListView scrollbar on item add/remove.

--- libraries/ZWidget/src/widgets/listview/listview.cpp
+++ libraries/ZWidget/src/widgets/listview/listview.cpp
@@ -8,11 +8,90 @@ ListView::ListView(Widget* parent) : Widget(parent)
 
 	scrollbar = new Scrollbar(this);
 	scrollbar->FuncScroll = [=]() { OnScrollbarScroll(); };
+
+	SetColumnWidths({ 0.0 });
 }
 
-void ListView::AddItem(const std::string& text)
+void ListView::SetColumnWidths(const std::vector<double>& widths)
 {
-	items.push_back(text);
+	columnwidths = widths;
+
+	bool updated = false;
+	const size_t newWidth = columnwidths.size();
+	for (std::vector<std::string>& column : items)
+	{
+		while (column.size() < newWidth)
+		{
+			updated = true;
+			column.push_back("");
+		}
+		while (column.size() > newWidth)
+		{
+			updated = true;
+			column.erase(column.end());
+		}
+	}
+
+	if (updated)
+		Update();
+}
+
+void ListView::AddItem(const std::string& text, int index, int column)
+{
+	if (column < 0 || column >= columnwidths.size())
+		return;
+
+	std::vector<std::string> newEntry;
+	for (size_t i = 0u; i < columnwidths.size(); ++i)
+		newEntry.push_back("");
+
+	newEntry[column] = text;
+	if (index >= 0)
+	{
+		if (index >= items.size())
+		{
+			newEntry[column] = "";
+			while (items.size() < index)
+				items.push_back(newEntry);
+
+			newEntry[column] = text;
+			items.push_back(newEntry);
+		}
+		else
+		{
+			items.insert(items.begin() + index, newEntry);
+		}
+	}
+	else
+	{
+		items.push_back(newEntry);
+	}
+	scrollbar->SetRanges(GetHeight(), items.size() * 20.0);
+	Update();
+}
+
+void ListView::UpdateItem(const std::string& text, int index, int column)
+{
+	if (index < 0 || index >= items.size() || column < 0 || column >= columnwidths.size())
+		return;
+
+	items[index][column] = text;
+	Update();
+}
+
+void ListView::RemoveItem(int index)
+{
+	if (!items.size() || index >= items.size())
+		return;
+
+	if (index < 0)
+		index = items.size() - 1;
+
+	if (selectedItem == index)
+		SetSelectedItem(0);
+
+	items.erase(items.begin() + index);
+	scrollbar->SetRanges(GetHeight(), items.size() * 20.0);
 	Update();
 }
 
@@ -68,7 +147,7 @@ void ListView::OnPaint(Canvas* canvas)
 	double h = 20.0;
 
 	int index = 0;
-	for (const std::string& item : items)
+	for (const std::vector<std::string>& item : items)
 	{
 		double itemY = y;
 		if (itemY + h >= 0.0 && itemY < GetHeight())
@@ -77,7 +156,12 @@ void ListView::OnPaint(Canvas* canvas)
 			{
 				canvas->fillRect(Rect::xywh(x - 2.0, itemY, w, h), Colorf::fromRgba8(100, 100, 100));
 			}
-			canvas->drawText(Point(x, y + 15.0), Colorf::fromRgba8(255, 255, 255), item);
+			double cx = x;
+			for (size_t entry = 0u; entry < item.size(); ++entry)
+			{
+				canvas->drawText(Point(cx, y + 15.0), Colorf::fromRgba8(255, 255, 255), item[entry]);
+				cx += columnwidths[entry];
+			}
 		}
 		y += h;
 		index++;

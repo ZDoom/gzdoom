d0e056565b44e94f5cb3f689b458a793809a14f0
Boondorl <boondorl@live.com>
2025-03-13 12:46:49 -0400

    Exported episode and skill infos

--- src/gamedata/g_mapinfo.h
+++ src/gamedata/g_mapinfo.h
@@ -41,6 +41,8 @@
 #include "screenjob.h"
 #include "hwrenderer/postprocessing/hw_postprocess.h"
 #include "hw_viewpointuniforms.h"
+#include "vm.h"
+#include "maps.h"
 
 struct level_info_t;
 struct cluster_info_t;
@@ -535,9 +537,9 @@ int G_SkillProperty(ESkillProperty prop);
 double G_SkillProperty(EFSkillProperty prop);
 const char * G_SkillName();
 
-typedef TMap<FName, FString> SkillMenuNames;
+typedef ZSMap<FName, FString> SkillMenuNames;
 
-typedef TMap<FName, FName> SkillActorReplacement;
+typedef ZSMap<FName, FName> SkillActorReplacement;
 
 struct FSkillInfo
 {

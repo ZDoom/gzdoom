5c1206ded0a30440b1507840953840b4036e8d81
MajorCooke <majorcooke22@gmail.com>
2025-03-30 18:15:53 -0500

    Exported: * GetLumpContainer * GetContainerName * GetLumpFullPath for WADS struct, useful for debugging custom-made parsers and identifying where problems may arise.
    All credit goes to Jay for the code.

--- src/common/scripting/interface/vmnatives.cpp
+++ src/common/scripting/interface/vmnatives.cpp
@@ -853,6 +853,27 @@ DEFINE_ACTION_FUNCTION(_Wads, GetLumpFullName)
 	ACTION_RETURN_STRING(fileSystem.GetFileFullName(lump));
 }
 
+DEFINE_ACTION_FUNCTION(_Wads, GetLumpContainer)
+{
+	PARAM_PROLOGUE;
+	PARAM_INT(lump);
+	ACTION_RETURN_INT(fileSystem.GetFileContainer(lump));
+}
+
+DEFINE_ACTION_FUNCTION(_Wads, GetContainerName)
+{
+	PARAM_PROLOGUE;
+	PARAM_INT(lump);
+	ACTION_RETURN_STRING(fileSystem.GetResourceFileName(lump));
+}
+
+DEFINE_ACTION_FUNCTION(_Wads, GetLumpFullPath)
+{
+	PARAM_PROLOGUE;
+	PARAM_INT(lump);
+	ACTION_RETURN_STRING(fileSystem.GetFileFullPath(lump));
+}
+
 DEFINE_ACTION_FUNCTION(_Wads, GetLumpNamespace)
 {
 	PARAM_PROLOGUE;

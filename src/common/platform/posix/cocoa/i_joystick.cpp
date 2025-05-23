55c7159a372e7c2c1c4b32bb68b938b3a356dd12
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-05-23 16:13:59 -0400

    Added osx I_Rumble stub

--- src/common/platform/posix/cocoa/i_joystick.cpp
+++ src/common/platform/posix/cocoa/i_joystick.cpp
@@ -1339,3 +1339,9 @@ CUSTOM_CVAR(Bool, joy_axespolling, true, CVAR_ARCHIVE | CVAR_GLOBALCONFIG | CVAR
 		s_joystickManager->UseAxesPolling(self);
 	}
 }
+
+void I_Rumble(double high_freq, double low_freq, double left_trig, double right_trig)
+{
+	// stub
+}
+

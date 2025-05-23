eeb6f53ba0ab178164ebe32ee094520063465b0e
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-05-23 16:49:10 -0400

    Moved from direct rumble, to key based rumble

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -2791,7 +2791,7 @@ static bool System_CaptureModeInGame()
 
 static void System_PlayStartupSound(const char* sndname)
 {
-	Joy_Rumble(6, 1.0, 1.0, 0.0, 0.0);
+	Joy_Rumble("misc/startupdone");
 	S_Sound(CHAN_BODY, 0, sndname, 1, ATTN_NONE);
 }
 
@@ -3549,7 +3549,7 @@ static int D_InitGame(const FIWADInfo* iwad_info, std::vector<std::string>& allw
 			autostart = true;
 		}
 
-		Joy_Rumble(6, 1.0, 1.0, 0.0, 0.0);
+		Joy_Rumble("misc/startupdone");
 		S_Sound (CHAN_BODY, 0, "misc/startupdone", 1, ATTN_NONE);
 
 		if (StartScreen)

34c2d4f917ef56a56dc656a9484911298792e504
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2025-07-30 01:25:09 -0400

    Squashed commit of the following:     - add submodule update scripts     - remove auto-setup scripts, they don't work anymore     - copy zmusic.dll into build folder (this may need to be also done on Linux sometime)     mirror changes from commit '194fb9e' at https://github.com/dpjudas/vkdoom     - fix for case sensitive systems

---
+++ update-subtrees.cmd
@@ -0,0 +1,3 @@
+@echo off
+git subtree pull --prefix=libraries/ZWidget https://github.com/dpjudas/ZWidget master --squash
+git subtree pull --prefix=libraries/ZMusic https://github.com/ZDoom/ZMusic master --squash

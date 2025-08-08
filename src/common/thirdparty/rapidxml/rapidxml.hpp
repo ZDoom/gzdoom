1bc56cf5565f596c3c92066d53895424dbc91f29
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-08-08 14:34:54 -0400

    Fixed windows build

--- src/common/thirdparty/rapidxml/rapidxml.hpp
+++ src/common/thirdparty/rapidxml/rapidxml.hpp
@@ -382,7 +382,7 @@ namespace rapidxml
     public:
 
         //! \cond internal
-        typedef void *(alloc_func)(std::size_t);       // Type of user-defined function used to allocate memory
+        typedef void *((alloc_func)(std::size_t));       // Type of user-defined function used to allocate memory
         typedef void (free_func)(void *);              // Type of user-defined function used to free memory
         //! \endcond
         
